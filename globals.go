c897881934df9803d051f308372eb400cd02a31e
Richard T. Carback III <rick.carback@gmail.com>
2018-01-02 12:49:50 -0800

    Cleanup globals by putting the round structure at the top and removing unncessary comments

--- globals.go
+++ globals.go
@@ -4,25 +4,6 @@ import (
 	"gitlab.com/privategrity/cyclic/int"
 )
 
-// Create and populates the arrays that hold all the keys for a
-// Node. Those keys are the R, S, T U,V, their associated private
-// keys. Also create structures for the base transmission and reception
-// keys.
-
-// A structure also needs to be created to store the Homomorphicly
-// Encrypted variants of R, S, T, U, and V.
-
-// A structure which is used to store the results of the Permuted Phase
-// in both the real time and the Precomputation is needed so that the
-// results of these phases can be outputted as a group to ensure no
-// information about the ordering is revealed.
-
-// Also needs to hold permutations
-
-// Keys for Homomorphic operations
-var Z *cyclic.Int // Node Cypher key
-var G *cyclic.Int // Global Generator
-
 // The round struct contains the keys and permutations for a given message batch
 type round struct {
 	R []*cyclic.Int // First unpermuted internode message key
@@ -41,5 +22,9 @@ type round struct {
 	Y_U []*cyclic.Int
 }
 
-// The Rounds map is a mapping of session identifiers to round structurs
-var Rounds map[string]round
+// Keys for Homomorphic operations
+var Z *cyclic.Int // Node Cypher key
+var G *cyclic.Int // Global Generator
+
+// The Rounds map is a mapping of session identifiers to round structures
+var Rounds map[string]*round
