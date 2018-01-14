f6656d8d136a422c5a18ab402b826686aa72eb64
Richard T. Carback III <rick.carback@gmail.com>
2018-01-14 13:11:14 -0800

    Add the correct imports so Identify builds, and fix misc syntax errors.

--- cryptops/realtime/identify.go
+++ cryptops/realtime/identify.go
@@ -2,42 +2,46 @@ package realtime
 
 import (
 	"gitlab.com/privategrity/crypto/cyclic"
+	"gitlab.com/privategrity/server/server"
+	"gitlab.com/privategrity/server/services"
 )
 
-type Identify struct{}
-
+type RealTimeIdentify struct{}
 
 // DispatchBuilder to perform the identify operation. This needs to
 // grab the aggregate U and V inverses so they can be multipied against
 // each message.
-func (self Identify) Build(group *cyclic.Group,
+func (self RealTimeIdentify) Build(group *cyclic.Group,
 	face interface{}) *services.DispatchBuilder {
 	round := face.(*server.Round)
 	batchSize := round.BatchSize
 	outMessages := make([]*services.Message, batchSize)
 	identifyMessageKeys := make([][]*cyclic.Int, batchSize)
 
-	for i, outMessage := range outMessages {
-		outMessage = services.NewMessage(i, 4, nil)
+	for i, _ := range outMessages {
+		outMessages[i] = services.NewMessage(uint64(i), 4, nil)
 		// NOTE: This seems wrong but I'm not sure how we fix it. FIXME when we link
 		//       everything up.
 		identifyMessageKeys[i] = []*cyclic.Int{
-			round.U_INV, round.V_INV}
+			round.U_INV[i], round.V_INV[i]}
 	}
 
 	return &services.DispatchBuilder{
 		BatchSize: batchSize,
-		Saved: identifyMessageKeys,
+		Saved: &identifyMessageKeys,
 		OutMessage: &outMessages,
 		G: group}
 }
 
-func (self Identify) Run(g *cyclic.Group, in, out *services.Message,
+func (self RealTimeIdentify) Run(g *cyclic.Group, in, out *services.Message,
 	saved *[]*cyclic.Int) *services.Message {
 	U_Inv, V_Inv := (*saved)[0], (*saved)[1]
 	EncryptedRecipient := in.Data[0]
 	DecryptedRecipient := out.Data[0]
+
 	Identify(g, EncryptedRecipient, DecryptedRecipient, U_Inv, V_Inv)
+
+	return out
 }
 
 // Identify (run only on the last node) multiplies the product of a
