864759ea714d90e33431eb0f7e8209458be1ce4d
Spencer Brown <spencer@privategrity.com>
2018-01-15 11:24:58 -0800

    Add realtime encrypt cryptop

---
+++ cryptops/realtime/encrypt_test.go
@@ -0,0 +1,12 @@
+package realtime
+
+import (
+	"gitlab.com/privategrity/crypto/cyclic"
+	"gitlab.com/privategrity/server/server"
+	"gitlab.com/privategrity/server/services"
+	"testing"
+)
+
+func TestRealtimeEncrypt(t *testing.T) {
+	t.Error("Test not implemented")
+}
