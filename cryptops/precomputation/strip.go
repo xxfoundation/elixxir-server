42324de83bc13bff5c98682bc8098a8ffd99c806
jaketaylor <jake@privategrity.com>
2018-01-15 09:30:17 -0800

    Update strip phase to remove PI_Z stuff per cMix document revision

--- cryptops/precomputation/strip.go
+++ cryptops/precomputation/strip.go
@@ -22,17 +22,7 @@ func (gen PrecompStrip) Build(g *cyclic.Group, face interface{}) *services.Dispa
 		om[i] = services.NewMessage(i, 3, nil)
 	}
 
-	var sav [][]*cyclic.Int
-
-	//Link the keys for stripping
-	for i := uint64(0); i < round.BatchSize; i++ {
-		roundSlc := []*cyclic.Int{
-			round.PI_Z,
-		}
-		sav = append(sav, roundSlc)
-	}
-
-	db := services.DispatchBuilder{BatchSize: round.BatchSize, Saved: &sav, OutMessage: &om, G: g}
+	db := services.DispatchBuilder{BatchSize: round.BatchSize, Saved: nil, OutMessage: &om, G: g}
 
 	return &db
 
@@ -40,45 +30,36 @@ func (gen PrecompStrip) Build(g *cyclic.Group, face interface{}) *services.Dispa
 
 func (gen PrecompStrip) Run(g *cyclic.Group, in, out *services.Message, saved *[]*cyclic.Int) *services.Message {
 
-	// Obtain PI_Z
-	PI_Z := (*saved)[0]
-
 	// Obtain message cypher text and encrypted message key
-	messageCypherText, encryptedMessageKey := in.Data[0], in.Data[1]
+	messageGlobalPrivateKey, encryptedMessageKey := in.Data[0], in.Data[1]
 	// Obtain recipient cypher text and encrypted recipient key
-	recipientCypherText, encryptedRecipientKey := in.Data[2], in.Data[3]
+	recipientGlobalPrivateKey, encryptedRecipientKey := in.Data[2], in.Data[3]
 
 	// Set output vars for the Message and Recipient Keys
 	// NOTE: Out index 2 used for temporary computation
 	messageKey, recipientKey, tmp := out.Data[0], out.Data[1], out.Data[2]
 
 	// Separate operations into helper function for testing
-	stripRunHelper(g, PI_Z, messageCypherText, encryptedMessageKey,
-		recipientCypherText, encryptedRecipientKey, messageKey, recipientKey, tmp)
+	stripRunHelper(g, messageGlobalPrivateKey, encryptedMessageKey,
+		recipientGlobalPrivateKey, encryptedRecipientKey, messageKey, recipientKey, tmp)
 
 	return out
 
 }
 
-func stripRunHelper(g *cyclic.Group, PI_Z, messageCypherText, encryptedMessageKey, recipientCypherText,
+func stripRunHelper(g *cyclic.Group, messageGlobalPrivateKey, encryptedMessageKey, recipientGlobalPrivateKey,
 	encryptedRecipientKey, messageKey, recipientKey, tmp *cyclic.Int) {
 	// Helper function for Precomp Strip Run
 
-	//Cancel PI_Z out of message cypher text to reveal global message private key
-	g.Root(messageCypherText, PI_Z, tmp)
-
 	// Invert the global message private key
-	g.Inverse(tmp, tmp)
+	g.Inverse(messageGlobalPrivateKey, tmp)
 
 	// Use the inverted global message private key to remove the homomorphic encryption
 	// from encrypted message key and reveal the message key
 	g.Mul(tmp, encryptedMessageKey, messageKey)
 
-	// Cancel PI_Z out of recipient cypher text to reveal global recipient private key
-	g.Root(recipientCypherText, PI_Z, tmp)
-
 	// Invert the global recipient private key
-	g.Inverse(tmp, tmp)
+	g.Inverse(recipientGlobalPrivateKey, tmp)
 
 	// Use the inverted global recipient private key to remove the homomorphic encryption
 	// from encrypted recipient key and reveal the recipient key
