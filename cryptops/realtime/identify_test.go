cb409374b7c6565ad06bdc8644bf925908f61712
Richard T. Carback III <rick.carback@gmail.com>
2018-01-24 21:00:30 +0000

    SlotIdentify test

--- cryptops/realtime/identify_test.go
+++ cryptops/realtime/identify_test.go
@@ -2,26 +2,39 @@ package realtime
 
 import (
 	"gitlab.com/privategrity/crypto/cyclic"
-
+	// "gitlab.com/privategrity/server/node"
+	// "gitlab.com/privategrity/server/services"
 	"testing"
 )
 
 // Smoke test test the identify function
 func TestIdentify(t *testing.T) {
-	RecipientPrecomp := cyclic.NewInt(42)
+	keys := KeysIdentify{
+		RecipientPrecomputation: cyclic.NewInt(42)}
 
 	grp := cyclic.NewGroup(cyclic.NewInt(43), cyclic.NewInt(5), cyclic.NewInt(23),
 		cyclic.NewRandom(cyclic.NewInt(1), cyclic.NewInt(42)))
 
-	EncryptedRecipient := cyclic.NewInt(42)
-	DecryptedRecipient := cyclic.NewInt(0)
+	// EncryptedRecipient := cyclic.NewInt(42)
+	// DecryptedRecipient := cyclic.NewInt(0)
+
+	im := SlotIdentify{
+		Slot: 0,
+		EncryptedRecipientID: cyclic.NewInt(42)}
+
+	om := SlotIdentify{
+		Slot: 0,
+		EncryptedRecipientID: cyclic.NewInt(0)}
+
 
 	ExpectedOutput := cyclic.NewInt(1) // 42*42 mod 43 => 1
 
-	Identify(&grp, EncryptedRecipient, DecryptedRecipient, RecipientPrecomp)
+	// Identify(&grp, EncryptedRecipient, DecryptedRecipient, RecipientPrecomp)
+	identify := Identify{}
+	identify.Run(&grp, &im, &om, &keys)
 
-	if DecryptedRecipient.Cmp(ExpectedOutput) != 0 {
+	if om.EncryptedRecipientID.Cmp(ExpectedOutput) != 0 {
 		t.Errorf("Expected: %v, Got: %v", ExpectedOutput.Text(10),
-			DecryptedRecipient.Text(10))
+			om.EncryptedRecipientID.Text(10))
 	}
 }
