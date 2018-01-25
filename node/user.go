6963d5bd02c9eaeff6d0886e0dbefa15ba6570a2
Spencer Brown <spencer@privategrity.com>
2018-01-24 16:49:18 -0800

    Add user and user_test from latest master

---
+++ node/user.go
@@ -0,0 +1,66 @@
+package node
+
+import (
+	"gitlab.com/privategrity/crypto/cyclic"
+)
+
+// Map which acts as a global registry that holds all users
+var userRegistry map[uint64]User
+
+// Sequential counter for user.Id values
+var idCounter uint64 = 0
+
+type ForwardKey struct {
+	BaseKey        *cyclic.Int
+	PartialBaseKey *cyclic.Int
+	RecursiveKey   *cyclic.Int
+}
+
+type User struct {
+	Id      uint64
+	Address string
+
+	Transmission ForwardKey
+	Reception    ForwardKey
+
+	PublicKey *cyclic.Int
+}
+
+// Initializes the userRegistry map
+func InitUserRegistry() {
+	userRegistry = make(map[uint64]User)
+}
+
+// Creates a new User object with default fields and given address
+func NewUser(address string) User {
+	idCounter++
+	return User{Id: idCounter - 1, Address: address,
+		Transmission: ForwardKey{BaseKey: cyclic.NewMaxInt(),
+			PartialBaseKey: cyclic.NewMaxInt(), RecursiveKey: cyclic.NewMaxInt()},
+		Reception: ForwardKey{BaseKey: cyclic.NewMaxInt(),
+			PartialBaseKey: cyclic.NewMaxInt(), RecursiveKey: cyclic.NewMaxInt()},
+		PublicKey: cyclic.NewMaxInt(),
+	}
+}
+
+// Deletes a user with the given ID from userRegistry
+func DeleteUser(id uint64) {
+	// If key does not exist, do nothing
+	delete(userRegistry, id)
+}
+
+// Returns a user with the given ID from userRegistry
+func GetUser(id uint64) User {
+	// If key does not exist, return nil
+	return userRegistry[id]
+}
+
+// Insert given user into userRegistry or update the user if it already exists (Upsert operation)
+func UpsertUser(user User) {
+	userRegistry[user.Id] = user
+}
+
+// Returns a count of the users in userRegistry
+func CountUsers() int {
+	return len(userRegistry)
+}
