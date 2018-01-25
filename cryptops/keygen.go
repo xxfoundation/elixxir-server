f844da30f7d11243388758b2891517d43a4a9c50
Spencer Brown <spencer@privategrity.com>
2018-01-25 12:24:54 -0800

    Initial implementation of cryptop run

--- cryptops/keygen.go
+++ cryptops/keygen.go
@@ -2,6 +2,7 @@ package cryptops
 
 import (
 	"gitlab.com/privategrity/crypto/cyclic"
+	"gitlab.com/privategrity/crypto/forward"
 	"gitlab.com/privategrity/server/node"
 	"gitlab.com/privategrity/server/services"
 )
@@ -12,17 +13,32 @@ type KeysGenerateClientKey struct {
 	sharedKeyStorage []byte
 }
 
-func (g GenerateClientKey) Run(group *cyclic.Group, in, out *KeySlot, keys *KeysGenerateClientKey) *KeySlot {
+func (g GenerateClientKey) Run(group *cyclic.Group, in, out KeySlot,
+	keys *KeysGenerateClientKey) KeySlot {
+	user := node.GetUser(in.UserID())
+
+	if in.GetKeyType() == TRANSMISSION {
+		forward.GenerateSharedKey(group, user.Transmission.BaseKey,
+			user.Transmission.RecursiveKey, out.Key(),
+			keys.sharedKeyStorage)
+	} else if in.GetKeyType() == RECEPTION {
+		forward.GenerateSharedKey(group, user.Reception.BaseKey,
+			user.Reception.RecursiveKey, out.Key(),
+			keys.sharedKeyStorage)
+	}
+
 	return out
 }
 
-func (g GenerateClientKey) Build(group *cyclic.Group, face interface{}) *services.DispatchBuilder {
+func (g GenerateClientKey) Build(group *cyclic.Group,
+	face interface{}) *services.DispatchBuilder {
 
 	// Get round from the empty interface
 	round := face.(*node.Round)
 
 	// Let's have 65536-bit long keys for now. We can increase or reduce
-	// size as needed after profiling
+	// size as needed after profiling, or perhaps look for a way to reuse
+	// these buffers more aggressively.
 	keys := make([]services.NodeKeys, round.BatchSize)
 	for i := uint64(0); i < round.BatchSize; i++ {
 		keySlc := &KeysGenerateClientKey{
