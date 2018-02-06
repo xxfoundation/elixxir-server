d7910f8b0bb013490d263c0c959b7b95e88af7ee
Spencer Brown <spencer@privategrity.com>
2018-02-05 16:49:19 -0800

    Add initial precomp encrypt server handler

--- io/precompEncrypt.go
+++ io/precompEncrypt.go
@@ -2,6 +2,10 @@ package io
 
 import (
 	pb "gitlab.com/privategrity/comms/mixmessages"
+	"gitlab.com/privategrity/comms/mixserver/message"
+	"gitlab.com/privategrity/crypto/cyclic"
+	"gitlab.com/privategrity/server/cryptops/precomputation"
+	"gitlab.com/privategrity/server/globals"
 	"gitlab.com/privategrity/server/services"
 )
 
@@ -9,9 +13,44 @@ import (
 type PrecompEncryptHandler struct{}
 
 // ReceptionHandler for PrecompEncryptMessages
-func (s ServerImpl) PrecompEncrypt(input *pb.PrecompEncryptMessage) {}
+func (s ServerImpl) PrecompEncrypt(input *pb.PrecompEncryptMessage) {
+	// Iterate through the Slots in the PrecompEncryptMessage
+	for i := 0; i < len(input.Slots); i++ {
+		// Convert input message to equivalent SlotEncrypt
+		in := input.Slots[i]
+		var slot services.Slot = &precomputation.SlotEncrypt{
+			Slot:                     in.Slot,
+			EncryptedMessageKeys:     cyclic.NewIntFromBytes(in.EncryptedMessageKeys),
+			PartialMessageCypherText: cyclic.NewIntFromBytes(in.PartialMessageCypherText),
+		}
+		// Pass slot as input to Encrypt's channel
+		s.GetChannel(input.RoundID, globals.PRECOMP_ENCRYPT) <- &slot
+	}
+}
 
 // TransmissionHandler for PrecompEncryptMessages
 func (h PrecompEncryptHandler) Handler(
 	roundId string, batchSize uint64, slots []*services.Slot) {
+	// Create the PrecompEncryptMessage
+	msg := &pb.PrecompEncryptMessage{
+		RoundID: roundId,
+		Slots:   make([]*pb.PrecompEncryptSlot, batchSize),
+	}
+
+	// Iterate over the output channel
+	for i := uint64(0); i < batchSize; i++ {
+		// Type assert Slot to SlotEncrypt
+		out := (*slots[i]).(*precomputation.SlotEncrypt)
+		// Convert to PrecompEncryptSlot
+		msgSlot := &pb.PrecompEncryptSlot{
+			Slot:                     out.Slot,
+			EncryptedMessageKeys:     out.EncryptedMessageKeys.Bytes(),
+			PartialMessageCypherText: out.PartialMessageCypherText.Bytes(),
+		}
+
+		// Put it into the slice
+		msg.Slots[i] = msgSlot
+	}
+	// Send the completed PrecompEncryptMessage
+	message.SendPrecompEncrypt(NextServer, msg)
 }
