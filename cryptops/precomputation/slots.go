6eb269a2ab329331969eb20c9be2a94268fe3072
Richard T. Carback III <rick.carback@gmail.com>
2018-02-20 18:16:12 +0000

    Replace all precomputation cryptop slots, except generate and share, with PrecomputationSlot, a general form of the slot structure

--- cryptops/precomputation/slots.go
+++ cryptops/precomputation/slots.go
@@ -1,6 +1,9 @@
 // Copyright © 2018 Privategrity Corporation
 //
 // All rights reserved.
+//
+// These are the dispatch slot types for precomputation dispatcher
+// implementations. There are 3 types: Generate, Share, and Precomputation.
 
 package precomputation
 
@@ -8,133 +11,46 @@ import (
 	"gitlab.com/privategrity/crypto/cyclic"
 )
 
-// These are all the dispatch slot types for precomputation dispatcher
-// implementations.
-
-// SlotGeneration is empty; no data being passed in or out of Generation
+// SlotGeneration is empty; no data being passed in or out of Generation.
+// The Generate dispatcher is a control signal to tell each node to
+// generate their keys for each message slot.
 type SlotGeneration struct {
-	//Slot Number of the Data
 	Slot uint64
 }
 
-// SlotShare is used to pass external data into Share and to pass the results out of Share
+// SlotShare is used to pass a round-wide Public Cypher Key to each node
 type SlotShare struct {
-	// Slot Number of the Data
 	Slot uint64
-	// Eq 10.3: Partial result of raising the global generator to the power of each node's Private Cypher Key
+	// Eq 10.3: Partial result of raising the global generator to the
+	//          power of each node's Private Cypher Key
 	PartialRoundPublicCypherKey *cyclic.Int
 }
 
-// SlotDecrypt is used to pass external data into Decrypt and to pass the results out of Decrypt
-type SlotDecrypt struct {
-	//Slot Number of the Data
+// PrecomputationSlot is a general slot structure used by all other
+// precomputation cryptops. The semantics of each element change and not
+// all elements are used by every cryptop, but the purpose remains the same
+// as the data travels through precomputation.
+type PrecomputationSlot struct {
 	Slot uint64
-	// Eq 12.9: First unpermuted internode message key from previous node
-	EncryptedMessageKeys *cyclic.Int
-	// Eq 12.11: First unpermuted internode recipient ID key from previous node
-	EncryptedRecipientIDKeys *cyclic.Int
-	// Eq 12.13: Partial cipher test for first unpermuted internode message key from previous node
-	PartialMessageCypherText *cyclic.Int
-	// Eq 12.15: Partial cipher test for first unpermuted internode recipient ID key from previous node
-	PartialRecipientIDCypherText *cyclic.Int
-}
-
-// SlotPermute is used to pass external data into Permute and to pass the
-// results out of Permute
-type SlotPermute struct {
-	//Slot Number of the Data
-	Slot uint64
-	// All of the first unpermuted internode message keys multiplied
-	// together under Homomorphic Encryption
-	EncryptedMessageKeys *cyclic.Int
-	// All of the unpermuted internode recipient keys multiplied together
-	// under Homomorphic Encryption
-	EncryptedRecipientIDKeys *cyclic.Int
-	// Partial Cypher Text for EncryptedMessageKeys
-	PartialMessageCypherText *cyclic.Int
-	// Partial Cypher Text for RecipientIDKeys
-	PartialRecipientIDCypherText *cyclic.Int
-}
-
-// SlotEncrypt is used to pass external data into Encrypt and to pass the results out of Encrypt
-type SlotEncrypt struct {
-	// Slot Number of the Data
-	Slot uint64
-	// Partial Precomputation for the Messages
-	EncryptedMessageKeys *cyclic.Int
-	// Partial Cypher Text for the Message Precomputation
-	PartialMessageCypherText *cyclic.Int
-}
-
-// (r Reveal) Run() uses SlotReveal structs to pass data into and out of Reveal
-type SlotReveal struct {
-	// Slot number
-	Slot uint64
-
-	// Partially decrypted message cypher texts
-	PartialMessageCypherText *cyclic.Int
-	// Partially decrypted recipient cypher texts
-	PartialRecipientCypherText *cyclic.Int
-}
-
-// SlotStripIn is used to pass external data into Strip
-type SlotStripIn struct {
-	//Slot Number of the Data
-	Slot uint64
-	// Encrypted but completed message precomputation
-	RoundMessagePrivateKey *cyclic.Int
-	// Encrypted but completed recipient precomputation
-	RoundRecipientPrivateKey *cyclic.Int
-}
-
-// SlotStripOut is used to pass the results out of Strip
-type SlotStripOut struct {
-	//Slot Number of the Data
-	Slot uint64
-	// Completed Message Precomputation
+	// Starts as message partial cypher text and becomes message precomputation
 	MessagePrecomputation *cyclic.Int
-	// Completed Recipient Precomputation
-	RecipientPrecomputation *cyclic.Int
+	// Encrypted message key to round message private key
+	MessageCypher *cyclic.Int
+	// Receiptient ID partial cypher text, becomes receipient id precomputation
+	RecipientIDPrecomputation *cyclic.Int
+	// Encrypted recipient id key to round recipient id private key
+	RecipientIDCypher *cyclic.Int
 }
 
-// SlotID functions for the above
-
-// SlotID Returns the Slot number
+// SlotID functions return the Slot number
 func (e *SlotGeneration) SlotID() uint64 {
 	return e.Slot
 }
 
-// SlotID Returns the Slot number
 func (e SlotShare) SlotID() uint64 {
 	return e.Slot
 }
 
-// SlotID Returns the Slot number of the input
-func (e *SlotStripIn) SlotID() uint64 {
+func (e *PrecomputationSlot) SlotID() uint64 {
 	return e.Slot
-}
-
-// SlotID Returns the Slot number of the output
-func (e *SlotStripOut) SlotID() uint64 {
-	return e.Slot
-}
-
-// SlotID Returns the Slot number
-func (e *SlotDecrypt) SlotID() uint64 {
-	return e.Slot
-}
-
-// SlotID Returns the Slot number
-func (e *SlotPermute) SlotID() uint64 {
-	return e.Slot
-}
-
-// SlotID Returns the Slot number
-func (e SlotEncrypt) SlotID() uint64 {
-	return e.Slot
-}
-
-// SlotID() gets the Slot number
-func (r *SlotReveal) SlotID() uint64 {
-	return r.Slot
 }
