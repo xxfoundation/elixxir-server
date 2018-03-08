4469cd4af3274892ae16568ddaf6a722e86f6281
jaketaylor <jake@privategrity.com>
2018-03-08 11:18:47 -0800

    Fix CMIX-185

--- io/askOnline.go
+++ io/askOnline.go
@@ -8,21 +8,20 @@ package io
 
 import (
 	jww "github.com/spf13/jwalterweatherman"
-	pb "gitlab.com/privategrity/comms/mixmessages"
 	"gitlab.com/privategrity/comms/clusterclient"
+	pb "gitlab.com/privategrity/comms/mixmessages"
 	"time"
 )
 
 // Blocks until all given servers respond
 func VerifyServersOnline(servers []string) {
 	for i := 0; i < len(servers); {
-		jww.DEBUG.Printf("Sending AskOnline message to %s...", servers[i])
 		_, err := clusterclient.SendAskOnline(servers[i], &pb.Ping{})
+		jww.INFO.Printf("Waiting for other federation servers (%d/%d)...",
+			i+1, len(servers))
 		if err != nil {
-			jww.ERROR.Printf("%v: Server %s failed to respond!", i, servers[i])
 			time.Sleep(250 * time.Millisecond)
 		} else {
-			jww.DEBUG.Printf("%v: Server %s responded!", i, servers[i])
 			i++
 		}
 	}
