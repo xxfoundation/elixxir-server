3c2c36e7e70c63c3c90d82387789f386d9e06db4
Spencer Brown <spencer@privategrity.com>
2018-03-08 11:26:53 -0800

    Fix test for request contact list

--- io/requestContactList_test.go
+++ io/requestContactList_test.go
@@ -14,9 +14,6 @@ import (
 )
 
 func TestRequestContactList(t *testing.T) {
-	// empty user registry from previous tests so we can make sure that our
-	// user's first in the map
-	globals.Users.DeleteUser(1)
 	user := globals.Users.NewUser("test") // Create user
 	user.Nick = "Michael"
 	globals.Users.UpsertUser(user) // Insert user into registry
@@ -31,12 +28,22 @@ func TestRequestContactList(t *testing.T) {
 	}
 
 	expectedNicks := []string{"Michael", "Me"}
+	nicksFound := make([]bool, len(expectedNicks))
+
 	for i := 0; i < len(expectedNicks); i++ {
-		if contacts.Contacts[i] == nil {
-			t.Errorf("Got a nil nick at index %v.", i)
-		} else if expectedNicks[i] != contacts.Contacts[i].Nick {
-			t.Errorf("Got an unexpected nick at index %v. Got: %v, "+
-				"expected: %v", i, contacts.Contacts[i].Nick, expectedNicks[i])
+		// look for the expected nick somewhere in the returned map
+		for j := 0; j < len(contacts.Contacts); j++ {
+			if contacts.Contacts[i] == nil {
+				t.Errorf("Got a nil nick at index %v.", i)
+			}
+			if contacts.Contacts[j].Nick == expectedNicks[i] {
+				nicksFound[i] = true
+				break
+			}
+		}
+		if !nicksFound[i] {
+			t.Errorf("Couldn't find the expected nick at index %v. "+
+				"expected: %v", i, expectedNicks[i])
 		}
 	}
 }
