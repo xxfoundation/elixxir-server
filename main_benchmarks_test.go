b85621d5ae842c104db232785e8b7efa6a9661f7
Richard T. Carback III <rick.carback@gmail.com>
2018-03-08 16:04:36 +0000

    Add all precomp benchmarks

--- main_benchmarks_test.go
+++ main_benchmarks_test.go
@@ -38,10 +38,9 @@ var PRIME = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
 		"93B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934063199" +
 		"FFFFFFFFFFFFFFFF"
 
-func BenchmarkGenerate(b *testing.B) {
-	nodeCount := 5
-	BatchSize := uint64(1024)
 
+// Tempate function for running the variations of GenerateRounds
+func RoundGeneratorBenchmark(nodeCount int, batchSize uint64, b *testing.B) {
 	prime := cyclic.NewInt(0)
 	prime.SetString(PRIME, 16)
 
@@ -51,24 +50,83 @@ func BenchmarkGenerate(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		GenerateRounds(nodeCount, BatchSize, &grp, b)
+		GenerateRounds(nodeCount, batchSize, &grp, b)
 	}
 }
 
-func BenchmarkPrecomp(b *testing.B) {
-	nodeCount := 5
-	BatchSize := uint64(1024)
+// GenerateRoundsBenchmarkTests
+func BenchmarkGenerateRounds_5_1024(b *testing.B) {
+	RoundGeneratorBenchmark(5, 1024, b)
+}
 
+
+// Template function for running precomputation
+func Precomp(nodeCount int, batchSize uint64, b *testing.B) {
+	if testing.Short() {
+		cnt := uint64(nodeCount) * batchSize
+		if cnt > 512 {
+			b.Skip("Skipping test due to short mode flag")
+		}
+	}
 	prime := cyclic.NewInt(0)
 	prime.SetString(PRIME, 16)
 
 	rng := cyclic.NewRandom(cyclic.NewInt(0), cyclic.NewInt(1000))
 	grp := cyclic.NewGroup(prime, cyclic.NewInt(5), cyclic.NewInt(4),
 		rng)
-	rounds := GenerateRounds(nodeCount, BatchSize, &grp, b)
+	rounds := GenerateRounds(nodeCount, batchSize, &grp, b)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		MultiNodePrecomp(nodeCount, BatchSize, &grp, rounds, b)
+		MultiNodePrecomp(nodeCount, batchSize, &grp, rounds, b)
 	}
 }
+
+func BenchmarkPrecomp_1_1(b *testing.B) { Precomp(1, 1, b) }
+func BenchmarkPrecomp_1_2(b *testing.B) { Precomp(1, 2, b) }
+func BenchmarkPrecomp_1_4(b *testing.B) { Precomp(1, 4, b) }
+func BenchmarkPrecomp_1_8(b *testing.B) { Precomp(1, 8, b) }
+func BenchmarkPrecomp_1_16(b *testing.B) { Precomp(1, 16, b) }
+func BenchmarkPrecomp_1_32(b *testing.B) { Precomp(1, 32, b) }
+func BenchmarkPrecomp_1_64(b *testing.B) { Precomp(1, 64, b) }
+func BenchmarkPrecomp_1_128(b *testing.B) { Precomp(1, 128, b) }
+func BenchmarkPrecomp_1_256(b *testing.B) { Precomp(1, 256, b) }
+func BenchmarkPrecomp_1_512(b *testing.B) { Precomp(1, 512, b) }
+func BenchmarkPrecomp_1_1024(b *testing.B) { Precomp(1, 1024, b) }
+
+func BenchmarkPrecomp_3_1(b *testing.B) { Precomp(3, 1, b) }
+func BenchmarkPrecomp_3_2(b *testing.B) { Precomp(3, 2, b) }
+func BenchmarkPrecomp_3_4(b *testing.B) { Precomp(3, 4, b) }
+func BenchmarkPrecomp_3_8(b *testing.B) { Precomp(3, 8, b) }
+func BenchmarkPrecomp_3_16(b *testing.B) { Precomp(3, 16, b) }
+func BenchmarkPrecomp_3_32(b *testing.B) { Precomp(3, 32, b) }
+func BenchmarkPrecomp_3_64(b *testing.B) { Precomp(3, 64, b) }
+func BenchmarkPrecomp_3_128(b *testing.B) { Precomp(3, 128, b) }
+func BenchmarkPrecomp_3_256(b *testing.B) { Precomp(3, 256, b) }
+func BenchmarkPrecomp_3_512(b *testing.B) { Precomp(3, 512, b) }
+func BenchmarkPrecomp_3_1024(b *testing.B) { Precomp(3, 1024, b) }
+
+func BenchmarkPrecomp_5_1(b *testing.B) { Precomp(5, 1, b) }
+func BenchmarkPrecomp_5_2(b *testing.B) { Precomp(5, 2, b) }
+func BenchmarkPrecomp_5_4(b *testing.B) { Precomp(5, 4, b) }
+func BenchmarkPrecomp_5_8(b *testing.B) { Precomp(5, 8, b) }
+func BenchmarkPrecomp_5_16(b *testing.B) { Precomp(5, 16, b) }
+func BenchmarkPrecomp_5_32(b *testing.B) { Precomp(5, 32, b) }
+func BenchmarkPrecomp_5_64(b *testing.B) { Precomp(5, 64, b) }
+func BenchmarkPrecomp_5_128(b *testing.B) { Precomp(5, 128, b) }
+func BenchmarkPrecomp_5_256(b *testing.B) { Precomp(5, 256, b) }
+func BenchmarkPrecomp_5_512(b *testing.B) { Precomp(5, 512, b) }
+func BenchmarkPrecomp_5_1024(b *testing.B) { Precomp(5, 1024, b) }
+
+func BenchmarkPrecomp_10_1(b *testing.B) { Precomp(10, 1, b) }
+func BenchmarkPrecomp_10_2(b *testing.B) { Precomp(10, 2, b) }
+func BenchmarkPrecomp_10_4(b *testing.B) { Precomp(10, 4, b) }
+func BenchmarkPrecomp_10_8(b *testing.B) { Precomp(10, 8, b) }
+func BenchmarkPrecomp_10_16(b *testing.B) { Precomp(10, 16, b) }
+func BenchmarkPrecomp_10_32(b *testing.B) { Precomp(10, 32, b) }
+func BenchmarkPrecomp_10_64(b *testing.B) { Precomp(10, 64, b) }
+func BenchmarkPrecomp_10_128(b *testing.B) { Precomp(10, 128, b) }
+func BenchmarkPrecomp_10_256(b *testing.B) { Precomp(10, 256, b) }
+func BenchmarkPrecomp_10_512(b *testing.B) { Precomp(10, 512, b) }
+func BenchmarkPrecomp_10_1024(b *testing.B) { Precomp(10, 1024, b) }
+
