643799e120c5fefbc98504958c65041fc10bda6a
Mario Costa <mario@privategrity.com>
2018-03-19 16:35:23 -0700

    Implemented cMix-132. Test for ThreadController.

---
+++ io/askOnline_test.go
@@ -0,0 +1,39 @@
+package io
+
+import (
+	"gitlab.com/privategrity/comms/clusterclient"
+	pb "gitlab.com/privategrity/comms/mixmessages"
+	"testing"
+	"time"
+)
+
+func TestVerifyServersOnline(t *testing.T) {
+
+	servers := []string{"13.56.70.255:11420"}
+
+	test := len(servers) + 1
+	pass := 0
+
+	for i := 0; i < len(servers); {
+		_, err := clusterclient.SendAskOnline(servers[i], &pb.Ping{})
+
+		if err != nil {
+			time.Sleep(250 * time.Millisecond)
+		} else {
+			i++
+			pass++
+		}
+	}
+
+	fakeServer := "SuperMario"
+	_, e := clusterclient.SendAskOnline(fakeServer, &pb.Ping{})
+
+	if e != nil {
+		pass++
+	} else {
+		t.Errorf("Error:Program connnected to a fake server!")
+	}
+
+	println("AskOnline test", pass, "out of", test, "tests passed.")
+
+}
