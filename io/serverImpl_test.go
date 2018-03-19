a4362a042a62682fa96d2d9c400240c18af2967b
jaketaylor <jake@privategrity.com>
2018-03-19 15:32:59 -0700

    Added test for SetPublicKey

--- io/serverImpl_test.go
+++ io/serverImpl_test.go
@@ -8,6 +8,7 @@ package io
 
 import (
 	"gitlab.com/privategrity/comms/mixserver"
+	"gitlab.com/privategrity/crypto/cyclic"
 	"gitlab.com/privategrity/server/globals"
 	"os"
 	"testing"
@@ -24,4 +25,22 @@ func TestMain(m *testing.M) {
 	// Next hop will be back to us
 	NextServer = localServer
 	os.Exit(m.Run())
+}
+
+func TestServerImpl_SetPublicKey(t *testing.T) {
+	roundId := "test"
+	expected := cyclic.NewInt(5)
+	globals.GlobalRoundMap = globals.NewRoundMap()
+	globals.GlobalRoundMap.AddRound(roundId,
+		globals.NewRound(5))
+
+	impl := ServerImpl{Rounds: &globals.GlobalRoundMap}
+	impl.SetPublicKey(roundId, expected.Bytes())
+
+	actual := impl.Rounds.GetRound(roundId).CypherPublicKey
+	if actual.Cmp(expected) != 0 {
+		t.Errorf("SetPublicKey: Values did not match!"+
+			" Expected %s Actual %s", expected.Text(10),
+			actual.Text(10))
+	}
 }
