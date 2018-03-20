be0e16ed9e5921d50be03480617ae756a59c59a1
Mario Costa <mario@privategrity.com>
2018-03-20 13:53:20 -0700

    Rewrote User_test.go file; Removed Lookup function from user.go in server; Added askOnline_test.go

--- io/askOnline_test.go
+++ io/askOnline_test.go
@@ -1,39 +1,26 @@
 package io
 
 import (
-	"gitlab.com/privategrity/comms/clusterclient"
-	pb "gitlab.com/privategrity/comms/mixmessages"
+	"net"
 	"testing"
-	"time"
 )
 
 func TestVerifyServersOnline(t *testing.T) {
 
-	// This is the (static) IP of the last node
-	servers := []string{"13.56.70.255:11420"}
+	// (Static) IP of the last node
+	server := "13.56.70.255:11420"
 
-	test := len(servers) + 1
+	test := 1
 	pass := 0
 
-	for i := 0; i < len(servers); {
-		_, err := clusterclient.SendAskOnline(servers[i], &pb.Ping{})
-
-		if err != nil {
-			time.Sleep(250 * time.Millisecond)
-		} else {
-			i++
-			pass++
-		}
-	}
-
-	fakeServer := "SuperMario"
-	_, e := clusterclient.SendAskOnline(fakeServer, &pb.Ping{})
-
-	if e != nil {
-		pass++
+	conn, err := net.Dial("tcp", server)
+	if err != nil {
+		t.Errorf("Error:Program could not connect to last node!")
 	} else {
-		t.Errorf("Error:Program connnected to a fake server!")
+		pass++
 	}
+
+	defer conn.Close()
 
 	println("AskOnline test", pass, "out of", test, "tests passed.")
 }
