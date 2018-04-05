c941e1ef70e3298f9c31754594527f0794ebe673
Richard T. Carback III <rick.carback@gmail.com>
2018-04-05 17:38:10 +0000

    Change WARN on debug phase printing to DEBUG line.

--- globals/round.go
+++ globals/round.go
@@ -148,7 +148,7 @@ func (round *Round) AddChannel(chanId Phase, newChan chan<- *services.Slot) {
 func (round *Round) WaitUntilPhase(phase Phase) {
 	round.phaseCond.L.Lock() // This must be held when calling wait
 	for round.phase < phase {
-		jww.WARN.Printf("Current Phase State: %s", round.phase.String())
+		jww.DEBUG.Printf("Current Phase State: %s", round.phase.String())
 		round.phaseCond.Wait()
 	}
 	round.phaseCond.L.Unlock()
