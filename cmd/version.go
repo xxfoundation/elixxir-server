f2b83851f1d6d35cfbcddd0ddfef7ff0da7a25ca
Richard T. Carback III <rick.carback@gmail.com>
2018-04-26 19:26:48 +0000

    Add a version subcommand

---
+++ cmd/version.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+  "fmt"
+  "github.com/spf13/cobra"
+)
+
+const GITVERSION = "PRIVATEGRITY_GIT_VERSION"
+const GLIDEDEPS = `PRIVATEGRITY_GLIDE_DEPENDENCIES`
+const SEMVER = "0.0.0a"
+
+func init() {
+  rootCmd.AddCommand(versionCmd)
+}
+
+func printVersion() {
+	fmt.Printf("Privategrity Server v%s -- %s\n\n", SEMVER, GITVERSION)
+	fmt.Printf("Dependencies:\n", GLIDEDEPS)
+}
+
+var versionCmd = &cobra.Command{
+  Use:   "version",
+  Short: "Print the version number of Privategrity Server",
+  Long:  `Print the version number of Privategrity Server. This also prints
+the glide cache versions of all of its dependencies.`,
+  Run: func(cmd *cobra.Command, args []string) {
+		printVersion()
+  },
+}
