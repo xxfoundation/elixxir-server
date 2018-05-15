a4092b296e723bf2ade200b1d9c9ee7cb9ba969c
willcarter <will@privategrity.com>
2018-05-14 20:17:39 -0700

    added a test and restored coverage metric threshold

--- io/roundtripPing_test.go
+++ io/roundtripPing_test.go
@@ -14,11 +14,21 @@ func TestGetRoundtripPing(t *testing.T) {
 	for i := range servers {
 		go node.StartServer(servers[i],
 			ServerImpl{Rounds: &globals.GlobalRoundMap})
-		if i == len(servers) - 1 {
+		if i == len(servers)-1 {
 			NextServer = servers[0]
 		} else {
 			NextServer = servers[i+1]
 		}
 	}
 	GetRoundtripPing(servers)
+}
+
+func TestGetRoundtripPing_OneServer(t *testing.T) {
+	server := []string{"localhost:50000"}
+	// Initialize GlobalRoundMap
+	globals.GlobalRoundMap = globals.NewRoundMap()
+	// Kick off comms server
+	go node.StartServer(server[0], ServerImpl{Rounds: &globals.GlobalRoundMap})
+	NextServer = server[0]
+	GetRoundtripPing(server)
 }
