753ae57e36a2a857110e72773161700f65500c68
Spencer Brown <spencer@privategrity.com>
2018-05-17 10:31:38 -0700

    Set and get NodeID in a better way

--- globals/nodeID.go
+++ globals/nodeID.go
@@ -3,28 +3,25 @@ package globals
 import (
 	jww "github.com/spf13/jwalterweatherman"
 	"github.com/spf13/viper"
-	"math"
+	"sync"
 )
 
 var IsLastNode bool
 
-var nodeId = uint64(math.MaxUint64)
+var nodeId uint64
+var setNodeIdOnce sync.Once
 
-func NodeID(serverIdx uint64) uint64 {
-	if nodeId == uint64(math.MaxUint64) {
-		viperNodeId := uint64(viper.GetInt("nodeID"))
-
-		// viper.IsSet() doesn't do what I want to do here, because binding it
-		// as a persistent flag in cmd/root.go always sets IsSet() to true
-		// for this flag. So, we check to see if the result from viper is 0
-		// instead. See https://github.com/spf13/viper/pull/331
-		if viperNodeId == 0 {
-			// set node ID to server index as a backup
-			nodeId = serverIdx
-		} else {
-			nodeId = viperNodeId
-		}
+func SetNodeID(newNodeID uint64) {
+	setNodeIdOnce.Do(func() {
+		nodeId = newNodeID
 		jww.DEBUG.Printf("Node ID: %v", nodeId)
-	}
+	})
+}
+
+func GetNodeID() uint64 {
+	setNodeIdOnce.Do(func() {
+		nodeId = uint64(viper.GetInt("nodeID"))
+		jww.WARN.Printf("Set node ID from Viper. Node ID: %v", nodeId)
+	})
 	return nodeId
 }
