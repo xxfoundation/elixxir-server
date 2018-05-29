a9e4099a7c342105ac339f2780b221c5d7d92b8c
Richard T. Carback III <rick.carback@gmail.com>
2018-05-29 21:37:56 +0000

    fix syntax error on recent fix

--- io/receiveMessage.go
+++ io/receiveMessage.go
@@ -32,7 +32,7 @@ func (s ServerImpl) ReceiveMessageFromClient(msg *pb.CmixMessage) {
 			Message:            messagePayload,
 			EncryptedRecipient: recipientId,
 		}
-		MessageCh <- &inputMsg:
+		MessageCh <- &inputMsg
 
 	} else {
 		jww.ERROR.Printf("Received message is not in the group: MsgPayload %v "+
