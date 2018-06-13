61cbdfb41eab3b4b1dc4c37cec8ef200d6192432
Benjamin <ben@privategrity.com>
2018-06-13 13:59:58 -0700

    removed changes to generate

--- cryptops/precomputation/generation.go
+++ cryptops/precomputation/generation.go
@@ -93,16 +93,11 @@ func (gen Generation) Run(g *cyclic.Group, in, out *SlotGeneration,
 	keys *KeysGeneration) services.Slot {
 
 	// Generates a random value within the group for every internode key
-	//g.Random(keys.R)
-	//g.Random(keys.S)
-	//g.Random(keys.T)
-	//g.Random(keys.U)
-	//g.Random(keys.V)
-	keys.R.SetUint64(uint64(5))
-	keys.S.SetUint64(uint64(5))
-	keys.T.SetUint64(uint64(5))
-	keys.U.SetUint64(uint64(5))
-	keys.V.SetUint64(uint64(5))
+	g.Random(keys.R)
+	g.Random(keys.S)
+	g.Random(keys.T)
+	g.Random(keys.U)
+	g.Random(keys.V)
 
 	// Generates the inverse keys
 	g.Inverse(keys.R, keys.R_INV)
@@ -113,25 +108,20 @@ func (gen Generation) Run(g *cyclic.Group, in, out *SlotGeneration,
 
 	// Generates a random value within the group for every private key
 
-	//yr, _ := cyclic.GenerateRandomKey(32)
-	//keys.Y_R.SetBytes(yr)
-	keys.Y_R.SetUint64(uint64(7))
+	yr, _ := cyclic.GenerateRandomKey(32)
+	keys.Y_R.SetBytes(yr)
 
-	//ys, _ := cyclic.GenerateRandomKey(16)
-	//keys.Y_S.SetBytes(ys)
-	keys.Y_S.SetUint64(uint64(7))
+	ys, _ := cyclic.GenerateRandomKey(16)
+	keys.Y_S.SetBytes(ys)
 
-	//yt, _ := cyclic.GenerateRandomKey(32)
-	//keys.Y_T.SetBytes(yt)
-	keys.Y_T.SetUint64(uint64(7))
+	yt, _ := cyclic.GenerateRandomKey(32)
+	keys.Y_T.SetBytes(yt)
 
-	//yu, _ := cyclic.GenerateRandomKey(32)
-	//keys.Y_U.SetBytes(yu)
-	keys.Y_U.SetUint64(uint64(7))
+	yu, _ := cyclic.GenerateRandomKey(32)
+	keys.Y_U.SetBytes(yu)
 
-	//yv, _ := cyclic.GenerateRandomKey(16)
-	//keys.Y_V.SetBytes(yv)
-	keys.Y_V.SetUint64(uint64(7))
+	yv, _ := cyclic.GenerateRandomKey(16)
+	keys.Y_V.SetBytes(yv)
 
 	return out
 
--- cryptops/precomputation/generation_test.go
+++ cryptops/precomputation/generation_test.go
@@ -49,70 +49,70 @@ func TestGeneration(t *testing.T) {
 		t.Errorf("Generation did not generate a coprime for Z, received: %v", round.Z.Text(10))
 		testOK = false
 	}
-	/*
-		// Only the most basic test of randomness is happening here
-		for i := uint64(0); i < batchSize-1; i++ {
-			if round.R[i].Cmp(round.R[i+1]) == 0 {
-				t.Errorf("Generation generated the same R between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.S[i].Cmp(round.S[i+1]) == 0 {
-				t.Errorf("Generation generated the same S between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.T[i].Cmp(round.T[i+1]) == 0 {
-				t.Errorf("Generation generated the same T between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.U[i].Cmp(round.U[i+1]) == 0 {
-				t.Errorf("Generation generated the same U between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.V[i].Cmp(round.V[i+1]) == 0 {
-				t.Errorf("Generation generated the same V between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.R_INV[i].Cmp(round.R_INV[i+1]) == 0 {
-				t.Errorf("Generation generated the same R_INV between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.S_INV[i].Cmp(round.S_INV[i+1]) == 0 {
-				t.Errorf("Generation generated the same S_INV between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.T_INV[i].Cmp(round.T_INV[i+1]) == 0 {
-				t.Errorf("Generation generated the same T_INV between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.U_INV[i].Cmp(round.U_INV[i+1]) == 0 {
-				t.Errorf("Generation generated the same U_INV between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.V_INV[i].Cmp(round.V_INV[i+1]) == 0 {
-				t.Errorf("Generation generated the same V_INV between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.Y_R[i].Cmp(round.Y_R[i+1]) == 0 {
-				t.Errorf("Generation generated the same Y_R between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.Y_S[i].Cmp(round.Y_S[i+1]) == 0 {
-				t.Errorf("Generation generated the same Y_S between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.Y_T[i].Cmp(round.Y_T[i+1]) == 0 {
-				t.Errorf("Generation generated the same Y_T between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.Y_U[i].Cmp(round.Y_U[i+1]) == 0 {
-				t.Errorf("Generation generated the same Y_U between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-			if round.Y_V[i].Cmp(round.Y_V[i+1]) == 0 {
-				t.Errorf("Generation generated the same Y_V between slots %d and %d\n", i, i+1)
-				testOK = false
-			}
-		}*/
+
+	// Only the most basic test of randomness is happening here
+	for i := uint64(0); i < batchSize-1; i++ {
+		if round.R[i].Cmp(round.R[i+1]) == 0 {
+			t.Errorf("Generation generated the same R between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.S[i].Cmp(round.S[i+1]) == 0 {
+			t.Errorf("Generation generated the same S between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.T[i].Cmp(round.T[i+1]) == 0 {
+			t.Errorf("Generation generated the same T between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.U[i].Cmp(round.U[i+1]) == 0 {
+			t.Errorf("Generation generated the same U between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.V[i].Cmp(round.V[i+1]) == 0 {
+			t.Errorf("Generation generated the same V between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.R_INV[i].Cmp(round.R_INV[i+1]) == 0 {
+			t.Errorf("Generation generated the same R_INV between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.S_INV[i].Cmp(round.S_INV[i+1]) == 0 {
+			t.Errorf("Generation generated the same S_INV between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.T_INV[i].Cmp(round.T_INV[i+1]) == 0 {
+			t.Errorf("Generation generated the same T_INV between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.U_INV[i].Cmp(round.U_INV[i+1]) == 0 {
+			t.Errorf("Generation generated the same U_INV between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.V_INV[i].Cmp(round.V_INV[i+1]) == 0 {
+			t.Errorf("Generation generated the same V_INV between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.Y_R[i].Cmp(round.Y_R[i+1]) == 0 {
+			t.Errorf("Generation generated the same Y_R between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.Y_S[i].Cmp(round.Y_S[i+1]) == 0 {
+			t.Errorf("Generation generated the same Y_S between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.Y_T[i].Cmp(round.Y_T[i+1]) == 0 {
+			t.Errorf("Generation generated the same Y_T between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.Y_U[i].Cmp(round.Y_U[i+1]) == 0 {
+			t.Errorf("Generation generated the same Y_U between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+		if round.Y_V[i].Cmp(round.Y_V[i+1]) == 0 {
+			t.Errorf("Generation generated the same Y_V between slots %d and %d\n", i, i+1)
+			testOK = false
+		}
+	}
 	if testOK {
 		pass++
 	}
