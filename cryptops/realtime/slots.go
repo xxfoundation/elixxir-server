b10f0ca926ce86654718cb25562a4b8b13539b83
Richard T. Carback III <rick.carback@gmail.com>
2018-06-28 17:06:01 +0000

    RealtimeSlot -> Slot

--- cryptops/realtime/slots.go
+++ cryptops/realtime/slots.go
@@ -6,7 +6,7 @@ import "gitlab.com/privategrity/crypto/cyclic"
 // realtime cryptops. The semantics of each element change and not
 // all elements are used by every cryptop, but the purpose remains the same
 // as the data travels through realtime.
-type RealtimeSlot struct {
+type Slot struct {
 	Slot uint64
 	// Encrypted RecipientID
 	EncryptedRecipient *cyclic.Int
@@ -16,9 +16,11 @@ type RealtimeSlot struct {
 	CurrentID uint64
 	// TransmissionKey, ReceptionKey, etc
 	CurrentKey *cyclic.Int
+	// Salt for client operations (only for Decrypt and Encrypt Phases)
+	Salt []byte
 }
 
 // SlotID functions return the Slot number
-func (e *RealtimeSlot) SlotID() uint64 {
+func (e *Slot) SlotID() uint64 {
 	return e.Slot
 }
