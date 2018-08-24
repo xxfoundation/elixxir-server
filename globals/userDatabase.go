51e488774eea9d4731665bcecff11d155cd6a74a
Spencer Brown <spencer@privategrity.com>
2018-08-24 10:47:51 -0700

    Update struct field name: maxAge -> maxConnAge

--- globals/userDatabase.go
+++ globals/userDatabase.go
@@ -53,7 +53,7 @@ func NewUserRegistry(username, password,
 		MaxRetries:  10,
 		PoolTimeout: time.Duration(2) * time.Minute,
 		IdleTimeout: time.Duration(10) * time.Minute,
-		MaxAge:      time.Duration(1) * time.Hour,
+		MaxConnAge:  time.Duration(1) * time.Hour,
 	})
 	// Attempt to connect to the database and initialize the schema
 	err := createSchema(db)
