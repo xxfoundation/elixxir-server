3028ee49e80d2969ecce0b451f37c66d6de613b6
Richard T. Carback III <rick.carback@gmail.com>
2019-03-11 09:15:43 +0000

    Add GetRoundBufferInfo function to server

--- io/gateway.go
+++ io/gateway.go
@@ -7,11 +7,14 @@
 package io
 
 import (
+	"errors"
 	jww "github.com/spf13/jwalterweatherman"
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/server/globals"
 	"time"
 )
+
+var GetRoundBufferInfoTimeout = "90s"
 
 // Start round receives a list of CmixMessages and sends them to the
 // ReceiveMessageFromClient handler.
@@ -38,4 +41,22 @@ func StartRound(messages *pb.InputMessages) {
 	endTime := time.Now()
 	jww.INFO.Printf("Finished StartRound(...) in %d ms",
 		(endTime.Sub(startTime))/time.Millisecond)
+}
+
+// GetRoundBufferInfo returns # of completed precomputations
+func GetRoundBufferInfo() (int, error) {
+	tout, _ := time.ParseDuration(GetRoundBufferInfoTimeout)
+	c := make(chan error, 1)
+	go func() {
+		for len(RoundCh) == 0 {
+			time.Sleep(250 * time.Millisecond)
+		}
+		c <- nil
+	}()
+	select {
+	case _ = <-c:
+		return len(RoundCh), nil
+	case <-time.After(tout):
+		return len(RoundCh), errors.New("Round buffer is empty!")
+	}
 }
