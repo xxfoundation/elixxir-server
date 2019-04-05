fc2f6c4f8f24f5adb99ab45b1e2d6272fd85586d
Benjamin Wenger <ben@elixxir.ioo>
2019-04-04 17:03:57 -0700

    updated message reception to properly check for a message in the wrong group

--- io/receiveMessage.go
+++ io/receiveMessage.go
@@ -9,6 +9,7 @@ package io
 import (
 	jww "github.com/spf13/jwalterweatherman"
 	pb "gitlab.com/elixxir/comms/mixmessages"
+	"gitlab.com/elixxir/crypto/large"
 	"gitlab.com/elixxir/primitives/id"
 	"gitlab.com/elixxir/server/cryptops/realtime"
 	"gitlab.com/elixxir/server/globals"
@@ -18,28 +19,37 @@ type ReceiveMessageHandler struct{}
 
 // Reception handler for ReceiveMessageFromClient
 func ReceiveMessageFromClient(msg *pb.CmixMessage) {
-	associatedData := globals.GetGroup().NewIntFromBytes(msg.AssociatedData)
-	messagePayload := globals.GetGroup().NewIntFromBytes(msg.MessagePayload)
+
+	grp := globals.GetGroup()
+
+	messagePayloadLarge := large.NewIntFromBytes(msg.MessagePayload)
+	associatedDataLarge := large.NewIntFromBytes(msg.AssociatedData)
+
+	//Check that the message is in the group and overwrite
+	if !grp.Inside(messagePayloadLarge) || !grp.Inside(associatedDataLarge) {
+		jww.ERROR.Printf("Message from client outside the group: %v %v",
+			messagePayloadLarge.Text(10), associatedDataLarge.Text(10))
+		associatedDataLarge.SetInt64(1)
+		messagePayloadLarge.SetInt64(1)
+	}
+
+	messagePayload := grp.NewIntFromLargeInt(messagePayloadLarge)
+	associatedData := grp.NewIntFromLargeInt(associatedDataLarge)
 
 	jww.DEBUG.Printf("Received message from client: %v",
 		messagePayload.Text(10))
 
-	// Verify message fields are within the global cyclic group
-	if globals.GetGroup().Inside(associatedData.GetLargeInt()) && globals.GetGroup().Inside(messagePayload.GetLargeInt()) {
-		// Convert message to a Slot
-		userId := new(id.User).SetBytes(msg.SenderID)
-		inputMsg := realtime.Slot{
-			Slot:           0, // Set in RunRealTime() in node/node.go
-			CurrentID:      userId,
-			Message:        messagePayload,
-			AssociatedData: associatedData,
-			CurrentKey:     globals.GetGroup().NewMaxInt(),
-			Salt:           msg.Salt,
-		}
-		MessageCh <- &inputMsg
-
-	} else {
-		jww.ERROR.Printf("Received message is not in the group: MsgPayload %v "+
-			"AssociatedData %v", messagePayload.Text(10), associatedData.Text(10))
+	// Convert message to a Slot
+	userId := new(id.User).SetBytes(msg.SenderID)
+	inputMsg := realtime.Slot{
+		Slot:           0, // Set in RunRealTime() in node/node.go
+		CurrentID:      userId,
+		Message:        messagePayload,
+		AssociatedData: associatedData,
+		CurrentKey:     globals.GetGroup().NewMaxInt(),
+		Salt:           msg.Salt,
 	}
+
+	MessageCh <- &inputMsg
+
 }
