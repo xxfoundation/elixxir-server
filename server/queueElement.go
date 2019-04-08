039760aab0a1d330d36ea6844d98947a6dd2920c
Benjamin Wenger <ben@elixxir.ioo>
2019-04-08 13:25:41 -0700

    Merge branch 'dispatch' of gitlab.com:elixxir/server into dispatchImproved
    # Conflicts:
    #	graphs/precomputation/decrypt.go
    #	services/dispatch_test.go
    #	services/graph.go

---
+++ server/queueElement.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"gitlab.com/elixxir/server/node"
+	"gitlab.com/elixxir/server/services"
+	"sync"
+)
+
+type queueElement struct {
+	g     *services.Graph
+	id    node.RoundID
+	phase node.Phase
+	loc   int
+	sync.Mutex
+}
+
+func (qe *queueElement) GetFingerprint() QueueFingerprint {
+	return makeGraphFingerprint(qe.id, qe.phase)
+}
