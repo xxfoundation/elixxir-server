33e113d4b2902163aada192bcfc30518dc9e4c8e
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-04-16 10:11:36 -0700

    Finished adding all tests to reveal

--- graphs/precomputation/reveal_test.go
+++ graphs/precomputation/reveal_test.go
@@ -1,6 +1,13 @@
 package precomputation
 
 import (
+	"fmt"
+	"gitlab.com/elixxir/crypto/cryptops"
+	"gitlab.com/elixxir/crypto/cyclic"
+	"gitlab.com/elixxir/crypto/large"
+	"gitlab.com/elixxir/server/graphs"
+	"gitlab.com/elixxir/server/node"
+	"gitlab.com/elixxir/server/services"
 	"testing"
 )
 
@@ -14,170 +21,114 @@ func TestRevealStream_GetName(t *testing.T) {
 		t.Errorf("RevealStream.GetName(), Expected %s, Recieved %s", expected, rs.GetName())
 	}
 }
-//
-//// Test that RevealStream.Link() Links correctly
-//func TestRevealStream_Link(t *testing.T) {
-//	primeString := "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
-//		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
-//		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
-//		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
-//		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
-//		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
-//		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
-//		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
-//		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
-//		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
-//		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
-//	grp := cyclic.NewGroup(large.NewIntFromString(primeString, 16), large.NewInt(2), large.NewInt(1283))
-//
-//	rs := RevealStream{}
-//
-//	batchSize := uint32(100)
-//
-//	round := node.NewRound(grp, 1, batchSize, batchSize)
-//
-//	rs.Link(batchSize, round)
-//
-//	checkStreamIntBuffer(grp, rs.R, round.R, "R", t)
-//	checkStreamIntBuffer(grp, rs.U, round.U, "U", t)
-//	checkStreamIntBuffer(grp, rs.R, round.R, "Y_R", t)
-//	checkStreamIntBuffer(grp, rs.U, round.U, "Y_U", t)
-//
-//	checkIntBuffer(rs.KeysMsg, batchSize, "KeysMsg", grp.NewInt(1), t)
-//	checkIntBuffer(rs.CypherMsg, batchSize, "CypherMsg", grp.NewInt(1), t)
-//	checkIntBuffer(rs.KeysAD, batchSize, "KeysAD", grp.NewInt(1), t)
-//	checkIntBuffer(rs.CypherAD, batchSize, "CypherAD", grp.NewInt(1), t)
-//}
-//
-//func TestRevealGraph(t *testing.T) {
-//	primeString := "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
-//		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
-//		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
-//		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
-//		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
-//		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
-//		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
-//		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
-//		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
-//		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
-//		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
-//	grp := cyclic.NewGroup(large.NewIntFromString(primeString, 16), large.NewInt(2), large.NewInt(1283))
-//
-//	batchSize := uint32(100)
-//
-//	expectedName := "PrecompReveal"
-//
-//	//Show that the Inti function meets the function type
-//	var graphInit graphs.Initializer
-//	graphInit = InitRevealGraph
-//
-//	//Initialize graph
-//	g := graphInit(func(err error) { return })
-//
-//	if g.GetName() != expectedName {
-//		t.Errorf("PrecompDecrypt has incorrect name Expected %s, Recieved %s", expectedName, g.GetName())
-//	}
-//
-//	//Build the graph
-//	g.Build(batchSize, services.AUTO_OUTPUTSIZE, 0)
-//
-//	//Build the round
-//	round := node.NewRound(grp, 1, g.GetBatchSize(), g.GetExpandedBatchSize())
-//
-//	//Link the graph to the round. building the stream object
-//	g.Link(round)
-//
-//	stream := g.GetStream().(*RevealStream)
-//
-//	//fill the fields of the stream object for testing
-//	grp.Random(stream.CypherPublicKey)
-//
-//	for i := uint32(0); i < g.GetExpandedBatchSize(); i++ {
-//		grp.Random(stream.R.Get(i))
-//		grp.Random(stream.U.Get(i))
-//		grp.Random(stream.Y_R.Get(i))
-//		grp.Random(stream.Y_U.Get(i))
-//	}
-//
-//	//Build i/o used for testing
-//	KeysMsgExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
-//	CypherMsgExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
-//	KeysADExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
-//	CypherADExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
-//
-//	//Run the graph
-//	g.Run()
-//
-//	//Send inputs into the graph
-//	go func(g *services.Graph) {
-//		for i := uint32(0); i < g.GetExpandedBatchSize(); i++ {
-//			g.Send(services.NewChunk(i, i+1))
-//		}
-//	}(g)
-//
-//	//Get the output
-//	s := g.GetStream().(*RevealStream)
-//
-//	for chunk := range g.ChunkDoneChannel() {
-//		for i := chunk.Begin(); i < chunk.End(); i++ {
-//			// Compute expected result for this slot
-//			cryptops.ElGamal(s.Grp, s.R.Get(i), s.Y_R.Get(i), s.CypherPublicKey, KeysMsgExpected.Get(i), CypherMsgExpected.Get(i))
-//			//Execute elgamal on the keys for the Associated Data
-//			cryptops.ElGamal(s.Grp, s.U.Get(i), s.Y_U.Get(i), s.CypherPublicKey, KeysADExpected.Get(i), CypherADExpected.Get(i))
-//
-//			if KeysMsgExpected.Get(i).Cmp(s.KeysMsg.Get(i)) != 0 {
-//				t.Error(fmt.Sprintf("PrecompDecrypt: Message Keys not equal on slot %v", i))
-//			}
-//			if CypherMsgExpected.Get(i).Cmp(s.CypherMsg.Get(i)) != 0 {
-//				t.Error(fmt.Sprintf("PrecompDecrypt: Message Keys Cypher not equal on slot %v", i))
-//			}
-//			if KeysADExpected.Get(i).Cmp(s.KeysAD.Get(i)) != 0 {
-//				t.Error(fmt.Sprintf("PrecompDecrypt: AD Keys not equal on slot %v", i))
-//			}
-//			if CypherADExpected.Get(i).Cmp(s.CypherAD.Get(i)) != 0 {
-//				t.Error(fmt.Sprintf("PrecompDecrypt: AD Keys Cypher not equal on slot %v", i))
-//			}
-//		}
-//	}
-//}
-//
-//func checkStreamIntBuffer(grp *cyclic.Group, ib, sourceib *cyclic.IntBuffer, source string, t *testing.T) {
-//	if ib.Len() != sourceib.Len() {
-//		t.Errorf("preomp.RevealStream.Link: Length of intBuffer %s not correct, "+
-//			"Expected %v, Recieved: %v", source, sourceib.Len(), ib.Len())
-//	}
-//
-//	numBad := 0
-//	for i := 0; i < sourceib.Len(); i++ {
-//		grp.SetUint64(sourceib.Get(uint32(i)), uint64(i))
-//		ci := ib.Get(uint32(i))
-//		if ci.Cmp(sourceib.Get(uint32(i))) != 0 {
-//			numBad++
-//		}
-//	}
-//
-//	if numBad != 0 {
-//		t.Errorf("preomp.RevealStream.Link: Ints in %v/%v intBuffer %s intilized incorrectly",
-//			numBad, sourceib.Len(), source)
-//	}
-//}
-//
-//func checkIntBuffer(ib *cyclic.IntBuffer, expandedBatchSize uint32, source string, defaultInt *cyclic.Int, t *testing.T) {
-//	if ib.Len() != int(expandedBatchSize) {
-//		t.Errorf("New RoundBuffer: Length of intBuffer %s not correct, "+
-//			"Expected %v, Recieved: %v", source, expandedBatchSize, ib.Len())
-//	}
-//
-//	numBad := 0
-//	for i := uint32(0); i < expandedBatchSize; i++ {
-//		ci := ib.Get(i)
-//		if ci.Cmp(defaultInt) != 0 {
-//			numBad++
-//		}
-//	}
-//
-//	if numBad != 0 {
-//		t.Errorf("New RoundBuffer: Ints in %v/%v intBuffer %s intilized incorrectly",
-//			numBad, expandedBatchSize, source)
-//	}
-//}
+
+// Test that RevealStream.Link() Links correctly
+func TestRevealStream_Link(t *testing.T) {
+	primeString := "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
+		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
+		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
+		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
+		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
+		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
+		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
+		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
+		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
+		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
+	grp := cyclic.NewGroup(large.NewIntFromString(primeString, 16), large.NewInt(2), large.NewInt(1283))
+
+	rs := RevealStream{}
+
+	batchSize := uint32(100)
+
+	round := node.NewRound(grp, 1, batchSize, batchSize)
+
+	rs.Link(batchSize, round)
+
+	if round.Z.Cmp(rs.Z) != 0 {
+		t.Errorf(
+			"RevealStream.Link() Z value not linked: Expected %s, Recieved %s",
+			round.Z.TextVerbose(10, 16), rs.Z.TextVerbose(10,16))
+	}
+
+	checkIntBuffer(rs.CypherMsg, batchSize, "CypherMsg", grp.NewInt(1), t)
+	checkIntBuffer(rs.CypherAD, batchSize, "CypherAD", grp.NewInt(1), t)
+}
+
+func TestReveal_Graph(t *testing.T) {
+	primeString :=
+		"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
+		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
+		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
+		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
+		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
+		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
+		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
+		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
+		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
+		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
+
+	grp := cyclic.NewGroup(large.NewIntFromString(primeString, 16), large.NewInt(2), large.NewInt(1283))
+
+	batchSize := uint32(100)
+
+	// Show that the Init function meets the function type
+	var graphInit graphs.Initializer
+	graphInit = InitRevealGraph
+
+	// Initialize graph
+	g := graphInit(func(err error) { return })
+
+	// Build the graph
+	g.Build(batchSize, services.AUTO_OUTPUTSIZE, 0)
+
+	// Build the round
+	round := node.NewRound(grp, 1, g.GetBatchSize(), g.GetExpandedBatchSize())
+
+	// Link the graph to the round. building the stream object
+	g.Link(round)
+
+	stream := g.GetStream().(*RevealStream)
+
+	// Fill the fields of the stream object for testing
+	grp.Random(stream.CypherPublicKey)
+
+	grp.Random(stream.Z)
+
+	// Build i/o used for testing
+	CypherMsgExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
+	CypherADExpected := grp.NewIntBuffer(g.GetExpandedBatchSize(), grp.NewInt(1))
+
+	// Run the graph
+	g.Run()
+
+	// Send inputs into the graph
+	go func(g *services.Graph) {
+		for i := uint32(0); i < g.GetExpandedBatchSize(); i++ {
+			g.Send(services.NewChunk(i, i+1))
+		}
+	}(g)
+
+	// Get the output
+	s := g.GetStream().(*RevealStream)
+
+	for chunk := range g.ChunkDoneChannel() {
+		for i := chunk.Begin(); i < chunk.End(); i++ {
+			// Compute expected result for this slot
+			cryptops.RootCoprime(s.Grp, CypherMsgExpected.Get(i), s.Z, CypherMsgExpected.Get(i))
+
+			// Execute root coprime on the keys for the Associated Data
+			cryptops.RootCoprime(s.Grp, CypherADExpected.Get(i), s.Z, CypherADExpected.Get(i))
+
+			if CypherMsgExpected.Get(i).Cmp(s.CypherMsg.Get(i)) != 0 {
+				t.Error(fmt.Sprintf("PrecompReveal: Message Keys Cypher not equal on slot %v", i))
+			}
+
+			if CypherADExpected.Get(i).Cmp(s.CypherAD.Get(i)) != 0 {
+				t.Error(fmt.Sprintf("PrecompReveal: AD Keys Cypher not equal on slot %v", i))
+			}
+		}
+	}
+}
+
