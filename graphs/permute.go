b7aca0290add3dc8395c45e1042f57d78a4e38ed
Benjamin Wenger <ben@elixxir.ioo>
2019-04-26 09:24:41 -0700

    removed most of permute

--- graphs/permute.go
+++ graphs/permute.go
@@ -7,7 +7,6 @@
 package graphs
 
 import (
-	"gitlab.com/elixxir/crypto/cryptops"
 	"gitlab.com/elixxir/crypto/cyclic"
 	"gitlab.com/elixxir/server/services"
 )
@@ -18,64 +17,9 @@ type PermuteIO struct {
 	Output []*cyclic.Int
 }
 
-// PermuteSubStream is used to store input and outputs slices for permutation
-type PermuteSubStream struct {
-	// Populate during Link
-	permutations []uint32
-	inputs       []*cyclic.IntBuffer
-
-	// New variable, created during Link
-	outputs [][]*cyclic.Int
-}
-
-// LinkStreams sets array of permutations slice references and appends each permute io from list into substream
-func (pss *PermuteSubStream) LinkPermuteSubStreams(expandedBatchSize uint32, permutation []uint32, ioLst ...PermuteIO) {
-
-	pss.permutations = permutation
-	for _, io := range ioLst {
-		pss.inputs = append(pss.inputs, io.Input)
-		pss.outputs = append(pss.outputs, io.Output)
-	}
-}
-
-type permuteSubStreamInterface interface {
-	getSubStream() *PermuteSubStream
-}
-
-func (pss *PermuteSubStream) getSubStream() *PermuteSubStream {
-	return pss
-}
-
-// Permute module implements slot permutations on Adapt and conforms to module interface using a dummy cryptop
-var Permute = services.Module{
-	Adapt: func(stream services.Stream, cryptop cryptops.Cryptop, chunk services.Chunk) error {
-		ps, ok := stream.(permuteSubStreamInterface)
-
-		if !ok {
-			return services.InvalidTypeAssert
-		}
-
-		pss := ps.getSubStream()
-
-		// do the permutations for the requested chunk
-		// rEaL iNtEnSe cRyPtOgRaPhy goInG oN hERe
-		for itr := range pss.inputs {
-			for j := chunk.Begin(); j < chunk.End(); j++ {
-				pss.outputs[itr][pss.permutations[j]] = pss.inputs[itr].Get(j)
-			}
-		}
-		return nil
-	},
-	Cryptop:        permuteDummyCryptop,
-	InputSize:      services.AUTO_INPUTSIZE,
-	StartThreshold: 1.0,
-	Name:           "Permute",
-	NumThreads:     services.AUTO_NUMTHREADS,
-}
-
 // ModifyGraphGeneratorForPermute makes a copy of the graph generator
 // where the OutputThreshold=1.0
-func ModifyGraphGeneratorForPermute(gc services.GraphGenerator)services.GraphGenerator{
+func ModifyGraphGeneratorForPermute(gc services.GraphGenerator) services.GraphGenerator {
 	return services.NewGraphGenerator(
 		gc.GetMinInputSize(),
 		gc.GetErrorHandler(),
@@ -83,19 +27,4 @@ func ModifyGraphGeneratorForPermute(gc services.GraphGenerator)services.GraphGen
 		gc.GetOutputSize(),
 		1.0,
 	)
-}
-
-/* Dummy cryptop for testing*/
-type permuteDummyCryptopPrototype func()
-
-var permuteDummyCryptop permuteDummyCryptopPrototype = func() { return }
-
-// Returns the name for debugging
-func (permuteDummyCryptopPrototype) GetName() string {
-	return "Permute Dummy Cryptop"
-}
-
-// Returns the input size, used in safety checks
-func (permuteDummyCryptopPrototype) GetInputSize() uint32 {
-	return 1
 }
