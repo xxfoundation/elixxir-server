f217a095a32c041323f5bb302b0031c63b511e1b
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-05-09 17:21:06 -0700

    Revert "Adding comments on impl for PostRoundPK and adding panic case into pk handler"
    This reverts commit 5a511642

--- node/impl.go
+++ node/impl.go
@@ -9,11 +9,16 @@
 package node
 
 import (
+	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/comms/node"
+	"gitlab.com/elixxir/primitives/id"
+	"gitlab.com/elixxir/server/io"
 	"gitlab.com/elixxir/server/server"
 	"gitlab.com/elixxir/server/server/round"
 	"gitlab.com/elixxir/server/server/phase"
 	"time"
+	"gitlab.com/elixxir/server/server/phase"
 )
 
 // NewImplementation creates a new implementation of the server.
@@ -30,12 +35,64 @@ func NewImplementation(instance *server.Instance) *node.Implementation {
 			time.Second)
 	}
 	// FIXME: Should handle error and return Ack
+	impl.Functions.PostPhase = func(batch *mixmessages.Batch) {
+		//Check if the operation can be done and get the correct phase if it can
+		_, p, err := rm.HandleIncomingComm(id.Round(batch.Round.ID), phase.Type(batch.ForPhase).String())
+		if err != nil {
+			jww.ERROR.Panicf("Error on comm, should be able to return: %+v", err)
+		}
 
-	impl.Functions.PostPhase = round.PostPhaseImpl(instance)
+		//queue the phase to be operated on if it is not queued yet
+		if p.AttemptTransitionToQueued() {
+			instance.GetResourceQueue().UpsertPhase(p)
+		}
+
+		//send the data to the phase
+		err = io.PostPhase(p, batch)
+		if err != nil {
+			jww.ERROR.Panicf("Error on PostPhase comm, should be able to return: %+v", err)
+		}
+	}
 
 	// Receive round public key from last node and sets it for the round for each node.
 	// Also starts precomputation decrypt phase with a batch
-	impl.Functions.PostRoundPublicKey = round.PostRoundPublicKeyImpl(instance)
+	impl.Functions.PostRoundPublicKey = func(pk *mixmessages.RoundPublicKey) {
+
+		tag := phase.Type(phase.PrecompShare).String() + "Verification"
+		r, p, err := rm.HandleIncomingComm(id.Round(pk.Round.ID), tag)
+		if err != nil {
+			jww.ERROR.Panicf("Error on comm, should be able to return: %+v", err)
+		}
+
+		err = io.PostRoundPublicKey(instance.GetGroup(), r.GetBuffer(), pk)
+		if err != nil {
+			jww.ERROR.Panicf("Error on PostRoundPublicKey comm, should be able to return: %+v", err)
+		}
+
+		instance.GetResourceQueue().DenotePhaseCompletion(p)
+
+		batchSize := r.GetBuffer().GetBatchSize()
+
+		if r.GetTopology().IsFirstNode(instance.GetID()) {
+			// Make fake batch
+			fakeBatch := &mixmessages.Batch{}
+
+			fakeBatch.Round = pk.Round
+			fakeBatch.ForPhase = int32(phase.PrecompDecrypt)
+			fakeBatch.Slots = make([]*mixmessages.Slot, batchSize)
+
+			for i := uint32(0); i < batchSize; i++ {
+				fakeBatch.Slots[i] = &mixmessages.Slot{
+					EncryptedMessageKeys:            []byte{1},
+					EncryptedAssociatedDataKeys:     []byte{1},
+					PartialMessageCypherText:        []byte{1},
+					PartialAssociatedDataCypherText: []byte{1},
+				}
+			}
+
+			impl.Functions.PostPhase(fakeBatch)
+		}
+	}
 
 	impl.Functions.GetCompletedBatch = func() (batch *mixmessages.Batch, e error) {
 		return io.GetCompletedBatch(instance.GetCompletedBatchQueue(), time.Second)
