897c3a47d1359d600ec6c5c2dbe45e8fdccd570c
Bernardo Cardoso <bernardo@elixxir.io>
2019-05-09 18:17:05 -0600

    Add registration io to new server

---
+++ io/registration.go
@@ -0,0 +1,143 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2019 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+// Package io registration.go handles the endpoints for registration
+
+package io
+
+import (
+	"bytes"
+	"crypto/rand"
+	"github.com/pkg/errors"
+	jww "github.com/spf13/jwalterweatherman"
+	hash2 "gitlab.com/elixxir/crypto/hash"
+	"gitlab.com/elixxir/crypto/large"
+	"gitlab.com/elixxir/crypto/nonce"
+	"gitlab.com/elixxir/crypto/registration"
+	"gitlab.com/elixxir/crypto/signature"
+	"gitlab.com/elixxir/server/server"
+)
+
+func RequestNonce(instance *server.Instance,
+	salt, Y, P, Q, G, hash, R, S []byte) ([]byte, error) {
+
+	grp := instance.GetGroup()
+	privKey := instance.GetPrivKey()
+
+	if !instance.GetSkipReg() {
+		// Verify signed public key using hardcoded RegistrationServer public key
+		valid := instance.GetRegPubKey().Verify(hash, signature.DSASignature{
+			R: large.NewIntFromBytes(R),
+			S: large.NewIntFromBytes(S),
+		})
+
+		// Concatenate Client public key byte slices
+		data := make([]byte, 0)
+		data = append(data, Y...)
+		data = append(data, P...)
+		data = append(data, Q...)
+		data = append(data, G...)
+
+		// Ensure that the data in the hash is identical to the Client public key
+		if !valid || !bytes.Equal(data, hash) {
+			// Invalid signed Client public key, return an error
+			jww.ERROR.Printf("Unable to verify signed public key!")
+			return make([]byte, 0), errors.Errorf("signed public key is invalid")
+		}
+	}
+
+	// Assemble Client public key
+	userPublicKey := signature.ReconstructPublicKey(
+		signature.CustomDSAParams(
+			large.NewIntFromBytes(P),
+			large.NewIntFromBytes(Q),
+			large.NewIntFromBytes(G)),
+		large.NewIntFromBytes(Y))
+
+	// Generate UserID
+	userId := registration.GenUserID(userPublicKey, salt)
+
+	// Generate a nonce with a timestamp
+	userNonce := nonce.NewNonce(nonce.RegistrationTTL)
+
+	// Generate user CMIX baseKey
+	b, _ := hash2.NewCMixHash()
+	baseKey := registration.GenerateBaseKey(grp, userPublicKey, privKey, b)
+
+	// Store user information in the database
+	newUser := instance.GetUserRegistry().NewUser(grp)
+	newUser.Nonce = userNonce
+	newUser.ID = userId
+	newUser.PublicKey = userPublicKey
+	newUser.BaseKey = baseKey
+	instance.GetUserRegistry().UpsertUser(newUser)
+
+	// Return nonce to Client with empty error field
+	return userNonce.Bytes(), nil
+}
+
+func ConfirmRegistration(instance *server.Instance,
+	hash, R, S []byte) ([]byte, []byte, []byte, []byte, []byte,
+	[]byte, []byte, error) {
+
+	// Obtain the user from the database
+	n := nonce.Nonce{}
+	copy(n.Value[:], hash)
+	user, err := instance.GetUserRegistry().GetUserByNonce(n)
+
+	if err != nil {
+		// Invalid nonce, return an error
+		jww.ERROR.Printf("Unable to find nonce: %x", n.Bytes())
+		return make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			make([]byte, 0), make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			errors.Errorf("nonce does not exist")
+	}
+
+	// Verify nonce has not expired
+	if !user.Nonce.IsValid() {
+		jww.ERROR.Printf("Nonce is expired: %x", n.Bytes())
+		return make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			make([]byte, 0), make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			errors.Errorf("nonce is expired")
+	}
+
+	// Verify signed nonce using Client public key
+	valid := user.PublicKey.Verify(hash, signature.DSASignature{
+		R: large.NewIntFromBytes(R),
+		S: large.NewIntFromBytes(S),
+	})
+
+	if !valid {
+		// Invalid signed nonce, return an error
+		jww.ERROR.Printf("Unable to verify nonce: %x", n.Bytes())
+		return make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			make([]byte, 0), make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			errors.Errorf("signed nonce is invalid")
+	}
+
+	// Concatenate Client public key byte slices
+	data := make([]byte, 0)
+	params := user.PublicKey.GetParams()
+	data = append(data, user.PublicKey.GetKey().Bytes()...)
+	data = append(data, params.GetP().Bytes()...)
+	data = append(data, params.GetQ().Bytes()...)
+	data = append(data, params.GetG().Bytes()...)
+
+	// Use hardcoded Server private key to sign Client public key
+	sig, err := instance.GetPrivKey().Sign(data, rand.Reader)
+	if err != nil {
+		// Unable to sign public key, return an error
+		jww.ERROR.Printf("Error signing client public key: %s", err)
+		return make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			make([]byte, 0), make([]byte, 0), make([]byte, 0), make([]byte, 0),
+			errors.New("unable to sign client public key")
+	}
+
+	grp := instance.GetGroup()
+	// Return signed Client public key to Client with empty error field
+	return data, sig.R.Bytes(), sig.S.Bytes(), instance.GetPubKey().GetKey().Bytes(),
+		grp.GetP().Bytes(), grp.GetQ().Bytes(), grp.GetG().Bytes(), nil
+}
---
+++ io/registration_test.go
@@ -0,0 +1,184 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2019 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package io
+
+import (
+	"gitlab.com/elixxir/crypto/cmix"
+	"gitlab.com/elixxir/crypto/csprng"
+	"gitlab.com/elixxir/crypto/cyclic"
+	"gitlab.com/elixxir/crypto/large"
+	"gitlab.com/elixxir/crypto/nonce"
+	"gitlab.com/elixxir/crypto/signature"
+	"gitlab.com/elixxir/server/globals"
+	"gitlab.com/elixxir/server/server"
+	"os"
+	"testing"
+	"time"
+)
+
+var serverInstance *server.Instance
+var dsaParams = signature.GetDefaultDSAParams()
+
+func TestMain(m *testing.M) {
+	grp := cyclic.NewGroup(dsaParams.GetP(), dsaParams.GetG(), dsaParams.GetQ())
+	serverInstance = server.CreateServerInstance(grp, &globals.UserMap{})
+	os.Exit(m.Run())
+}
+
+// Test request nonce
+func TestRequestNonce(t *testing.T) {
+	regPrivKey := signature.ReconstructPrivateKey(serverInstance.GetRegPubKey(),
+		large.NewIntFromString("dab0febfab103729077ad4927754f6390e366fdf4c58e8d40dadb3e94c444b54", 16))
+	rng := csprng.NewSystemRNG()
+	privKey := dsaParams.PrivateKeyGen(rng)
+	pubKey := privKey.PublicKeyGen()
+	salt := cmix.NewSalt(rng, 32)
+
+	hash := append(pubKey.GetKey().Bytes(), dsaParams.GetP().Bytes()...)
+	hash = append(hash, dsaParams.GetQ().Bytes()...)
+	hash = append(hash, dsaParams.GetG().Bytes()...)
+
+	sign, err := regPrivKey.Sign(hash, rng)
+	if sign == nil || err != nil {
+		t.Errorf("Error signing data: %v", err.Error())
+	}
+
+	result, err2 := RequestNonce(serverInstance,
+		salt,
+		pubKey.GetKey().Bytes(),
+		dsaParams.GetP().Bytes(),
+		dsaParams.GetQ().Bytes(),
+		dsaParams.GetG().Bytes(),
+		hash,
+		sign.R.Bytes(),
+		sign.S.Bytes())
+
+	if result == nil || err2 != nil {
+		t.Errorf("Error in RequestNonce")
+	}
+}
+
+
+// Test request nonce with invalid signature
+func TestRequestNonce_BadSignature(t *testing.T) {
+	rng := csprng.NewSystemRNG()
+	privKey := dsaParams.PrivateKeyGen(rng)
+	pubKey := privKey.PublicKeyGen()
+	salt := cmix.NewSalt(rng, 32)
+	regPrivKey := dsaParams.PrivateKeyGen(rng)
+
+	hash := append(pubKey.GetKey().Bytes(), dsaParams.GetP().Bytes()...)
+	hash = append(hash, dsaParams.GetQ().Bytes()...)
+	hash = append(hash, dsaParams.GetG().Bytes()...)
+
+	sign, err := regPrivKey.Sign(hash, rng)
+	if sign == nil || err != nil {
+		t.Errorf("Error signing data")
+	}
+
+	_, err2 := RequestNonce(serverInstance,
+		salt,
+		pubKey.GetKey().Bytes(),
+		dsaParams.GetP().Bytes(),
+		dsaParams.GetQ().Bytes(),
+		dsaParams.GetG().Bytes(),
+		hash,
+		sign.R.Bytes(),
+		sign.S.Bytes())
+
+	if err2 == nil {
+		t.Errorf("Expected error in RequestNonce")
+	}
+}
+
+// Test confirm nonce
+func TestConfirmNonce(t *testing.T) {
+	user := serverInstance.GetUserRegistry().NewUser(serverInstance.GetGroup())
+	user.Nonce = nonce.NewNonce(nonce.RegistrationTTL)
+	serverInstance.GetUserRegistry().UpsertUser(user)
+
+	rng := csprng.NewSystemRNG()
+	privKey := dsaParams.PrivateKeyGen(rng)
+	pubKey := privKey.PublicKeyGen()
+	user.PublicKey = pubKey
+
+	sign, err := privKey.Sign(user.Nonce.Bytes(), rng)
+	if sign == nil || err != nil {
+		t.Errorf("Error signing data")
+	}
+
+	_, _, _, _, _, _, _, err2 := ConfirmRegistration(serverInstance,
+		user.Nonce.Bytes(), sign.R.Bytes(), sign.S.Bytes())
+	if err2 != nil {
+		t.Errorf("Error in ConfirmNonce")
+	}
+}
+
+// Test confirm nonce that doesn't exist
+func TestConfirmNonce_NonExistant(t *testing.T) {
+	user := serverInstance.GetUserRegistry().NewUser(serverInstance.GetGroup())
+	user.Nonce = nonce.NewNonce(nonce.RegistrationTTL)
+
+	rng := csprng.NewSystemRNG()
+	privKey := dsaParams.PrivateKeyGen(rng)
+	pubKey := privKey.PublicKeyGen()
+	user.PublicKey = pubKey
+
+	sign, err := privKey.Sign(user.Nonce.Bytes(), rng)
+	if sign == nil || err != nil {
+		t.Errorf("Error signing data")
+	}
+
+	_, _, _, _, _, _, _, err2 := ConfirmRegistration(serverInstance,
+		user.Nonce.Bytes(), sign.R.Bytes(), sign.S.Bytes())
+	if err2 == nil {
+		t.Errorf("ConfirmNonce: Expected unexistant nonce")
+	}
+}
+
+// Test confirm nonce expired
+func TestConfirmNonce_Expired(t *testing.T) {
+	user := serverInstance.GetUserRegistry().NewUser(serverInstance.GetGroup())
+	user.Nonce = nonce.NewNonce(1)
+	serverInstance.GetUserRegistry().UpsertUser(user)
+
+	rng := csprng.NewSystemRNG()
+	privKey := dsaParams.PrivateKeyGen(rng)
+	pubKey := privKey.PublicKeyGen()
+	user.PublicKey = pubKey
+
+	sign, err := privKey.Sign(user.Nonce.Bytes(), rng)
+	if sign == nil || err != nil {
+		t.Errorf("Error signing data")
+	}
+
+	// Wait for nonce to expire
+	wait := time.After(time.Duration(2) * time.Second)
+	select {
+	case <-wait:
+	}
+
+	_, _, _, _, _, _, _, err2 := ConfirmRegistration(serverInstance,
+		user.Nonce.Bytes(), sign.R.Bytes(), sign.S.Bytes())
+	if err2 == nil {
+		t.Errorf("ConfirmNonce: Expected expired nonce")
+	}
+}
+
+// Test confirm nonce with invalid signature
+func TestConfirmNonce_BadSignature(t *testing.T) {
+	user := serverInstance.GetUserRegistry().NewUser(serverInstance.GetGroup())
+	user.Nonce = nonce.NewNonce(nonce.RegistrationTTL)
+	serverInstance.GetUserRegistry().UpsertUser(user)
+
+	_, _, _, _, _, _, _, err := ConfirmRegistration(serverInstance,
+		user.Nonce.Bytes(), make([]byte, 0),
+		make([]byte, 0))
+	if err == nil {
+		t.Errorf("ConfirmNonce: Expected bad signature!")
+	}
+}
