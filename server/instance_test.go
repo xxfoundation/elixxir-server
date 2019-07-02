d8449c574e0071592270af90d1f7fdf71794c453
Richard Carback <rick@elixxir.io>
2019-07-01 21:08:19 -0400

    Add tests for topology util funcs

--- server/instance_test.go
+++ server/instance_test.go
@@ -3,6 +3,8 @@ package server
 import (
 	"gitlab.com/elixxir/comms/node"
 	"gitlab.com/elixxir/crypto/cyclic"
+	"gitlab.com/elixxir/primitives/circuit"
+	"gitlab.com/elixxir/primitives/id"
 	"gitlab.com/elixxir/crypto/large"
 	"gitlab.com/elixxir/server/globals"
 	"gitlab.com/elixxir/server/server/conf"
@@ -10,22 +12,31 @@ import (
 	"testing"
 )
 
-func TestInstance_GetGroup(t *testing.T) {
-	primeString := "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
-		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
-		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
-		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
-		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
-		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
-		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
-		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
-		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
-		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
-		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
-	grp := cyclic.NewGroup(large.NewIntFromString(primeString, 16), large.NewInt(2), large.NewInt(1283))
-	i := mockServerInstance(grp)
+const MODP768 = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
+	"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
+	"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
+	"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
+	"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
+	"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
+	"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+	"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
+	"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
+	"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
+	"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
 
-	if !reflect.DeepEqual(i.GetGroup(), grp) {
+var instance *Instance
+
+
+func TestMain(m *testing.M) {
+	prime := large.NewIntFromString(MODP768, 16)
+	grp  := cyclic.NewGroup(prime, large.NewInt(2), large.NewInt(1283))
+	instance = mockServerInstance(grp)
+}
+
+func TestInstance_GetGroup(t *testing.T) {
+	prime := large.NewIntFromString(MODP768, 16)
+	grp  := cyclic.NewGroup(prime, large.NewInt(2), large.NewInt(1283))
+	if instance.GetGroup().GetFingerprint() != grp.GetFingerprint() {
 		t.Errorf("Instance.GetGroup: Returned incorrect group")
 	}
 }
@@ -59,6 +70,34 @@ func TestInstance_GetID(t *testing.T) {
 	if !reflect.DeepEqual(i.GetID(), nid) {
 		t.Errorf("Instance.GetID: Returned incorrect " +
 			"ID")
+	}
+}
+
+func TestInstance_Topology(t *testing.T) {
+	var nodeIDs []*id.Node
+
+	//Build IDs
+	for i := 0; i < 3; i++ {
+		nodIDBytes := make([]byte, id.NodeIdLen)
+		nodIDBytes[0] = byte(i + 1)
+		nodeID := id.NewNodeFromBytes(nodIDBytes)
+		nodeIDs = append(nodeIDs, nodeID)
+	}
+
+	//Build the topology
+	top := circuit.New(nodeIDs)
+	i := &Instance{topology: top}
+
+	if !reflect.DeepEqual(i.GetTopology(), t) {
+		t.Errorf("Instance.GetTopology: Returned incorrect " +
+			"Topology")
+	}
+
+	if i.IsFirstNode() {
+		t.Errorf("I am not first node!")
+	}
+	if !i.IsLastNode() {
+		t.Errorf("I should be last node!")
 	}
 }
 
