18c586a5d98ce183d3822d76e961e24c828c6332
jaketaylor <jake@privategrity.com>
2019-07-23 14:19:33 -0700

    Removed registration dependency on hash and improved logs; Updated config file to pass in permissioning public key

--- io/registration_test.go
+++ io/registration_test.go
@@ -40,6 +40,16 @@ func TestMain(m *testing.M) {
 		Node: conf.Node{
 			Ids: []string{nid.String()},
 		},
+		Permissioning: conf.Permissioning{
+			PublicKey: "-----BEGIN PUBLIC KEY-----\n" +
+				"GuP9Tpgp+0ZEWeBbyjkr7FBnFS+0Olaa08O2i7ythPD/jTHHZ9o+q8/Ahw2Cs5V" +
+				"oYQtS8rcrSTu+3m6VLJp/1EqBYeYqkEaCjEpl9AGy8FTr9zduidq1R9ijw9Roke" +
+				"eKz8QBVxPL+1sLbKsPjftGuJHzVCBGrOTKuYTV3+9PUtQ0fcflL2p+qFHdoHbw7" +
+				"R/vhuxrXCpIBxSZBr+OC/cLMBFH/qiP2VAJ7fvg3o/8GoZOSzokJlthocR6TpMH" +
+				"58hPm1WRdltTD1hZ+peyLOm1E4XT0TCIeVsvn9DLWTV/6Tg0YRffKs8rqyLZQt4" +
+				"acOjV1i/A6Z2HQqDxbflM46Cruw==\n" +
+				"-----END PUBLIC KEY-----\n",
+		},
 	}
 
 	serverInstance = server.CreateServerInstance(&params, &globals.UserMap{},
