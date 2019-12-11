edad40963dd5b9a85cf61ccc282073d27575b608
Konstantino Sparakis <ksparakis@elixxir.io>
2019-12-11 14:10:28 -0500

    changes

--- services/assignment_test.go
+++ services/assignment_test.go
@@ -1,1 +1,25 @@
 package services
+
+import "testing"
+
+
+func TestAssignment_Enqueue(t *testing.T) {
+	a := newAssignment(100)
+	k, err := a.Enqueue(102,101)
+
+	if err != nil{
+		//error here should equal nil
+	}
+}
+
+func TestAssignment_GetChunk(t *testing.T) {
+
+}
+
+func TestAssignmentList_DenoteCompleted(t *testing.T) {
+
+}
+
+func TestAssignmentList_PrimeOutputs(t *testing.T) {
+
+}
