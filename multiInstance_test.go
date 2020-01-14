0271fab691ea0d6425e93726a60c67c2d2c2419b
jbhusson <jonah@elixxir.io>
2020-01-14 14:44:00 -0800

    fix multiinstance test

--- multiInstance_test.go
+++ multiInstance_test.go
@@ -180,7 +180,7 @@ func MultiInstanceTest(numNodes, batchsize int, t *testing.T) {
 
 	//wait for last node to be ready to receive the batch
 	completedBatch := &mixmessages.Batch{Slots: make([]*mixmessages.Slot, 0)}
-	h, _ := connect.NewHost("test", "test", nil, false, false)
+	h, _ := connect.NewHost(lastNode.GetID().NewGateway().String(), "test", nil, false, false)
 	for len(completedBatch.Slots) == 0 {
 		completedBatch, _ = io.GetCompletedBatch(lastNode, 100*time.Millisecond, &connect.Auth{
 			IsAuthenticated: true,
