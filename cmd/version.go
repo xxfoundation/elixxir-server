198e64b2f9bf93ea1d366553b57d5ccb206adb43
jaketaylor <jake@privategrity.com>
2020-06-24 12:33:06 -0700

    update vers

--- cmd/version.go
+++ cmd/version.go
@@ -16,7 +16,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "1.2.1"
+const currentVersion = "1.2.2"
 
 func printVersion() {
 	fmt.Printf("xx network Server v%s -- %s\n\n", SEMVER, GITVERSION)
