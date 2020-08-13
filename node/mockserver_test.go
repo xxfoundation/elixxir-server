bd471d833e98ff2a3e4a9b4e977672d6d14ec65b
josh <josh@elixxir.io>
2020-08-13 13:04:58 -0700

    Fix pipeline

--- node/mockserver_test.go
+++ node/mockserver_test.go
@@ -261,6 +261,20 @@ func (*mockGateway) Poll(*pb.GatewayPoll) (*pb.GatewayPollResponse, error) {
 	return nil, nil
 }
 
+func (*mockGateway) RequestHistoricalRounds(msg *pb.HistoricalRounds) (*pb.HistoricalRoundsResponse, error) {
+	return &pb.HistoricalRoundsResponse{}, nil
+}
+
+// Client -> Gateway message request
+func (*mockGateway) RequestMessages(msg *pb.GetMessages) (*pb.GetMessagesResponse, error) {
+	return &pb.GetMessagesResponse{}, nil
+}
+
+// Client -> Gateway bloom request
+func (*mockGateway) RequestBloom(msg *pb.GetBloom) (*pb.GetBloomResponse, error) {
+	return &pb.GetBloomResponse{}, nil
+}
+
 func mockServerDef(i interface{}) *internal.Definition {
 	nid := internal.GenerateId(i)
 
