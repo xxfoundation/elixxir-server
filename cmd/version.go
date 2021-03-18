115144388d70e5a9f79d229013dfe3d54eed724e
Jake Taylor <jake@elixxir.io>
2021-03-18 16:19:04 -0500

    update ver

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.1.0"
+const currentVersion = "2.2.0"
 
 func printVersion() {
 	fmt.Printf("xx network Server v%s -- %s\n\n", SEMVER, GITVERSION)
