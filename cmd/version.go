63642922beb9f2831f9cb9574efca6a8a9d38b94
Benjamin Wenger <ben@elixxir.ioo>
2021-03-24 09:33:06 -0700

    incrememented version

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.2.3"
+const currentVersion = "2.2.4"
 
 func printVersion() {
 	fmt.Printf("xx network Server v%s -- %s\n\n", SEMVER, GITVERSION)
