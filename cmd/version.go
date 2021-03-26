862e70ac5f73716e189369108516853701073d03
Richard T. Carback III <rick.carback@gmail.com>
2021-03-26 18:09:43 +0000

    Update version

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.2.4"
+const currentVersion = "2.2.5"
 
 func printVersion() {
 	fmt.Printf("xx network Server v%s -- %s\n\n", SEMVER, GITVERSION)
