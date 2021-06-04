19cf878fe6e91382925db5c9adf4419a1997d240
Benjamin Wenger <ben@elixxir.ioo>
2021-06-04 10:27:59 -0700

    diferent config

--- io/transmitPhasestream.go
+++ io/transmitPhasestream.go
@@ -28,8 +28,8 @@ import (
 	"time"
 )
 
-const blockSize = 4
-const numStreams = 75
+const blockSize = 1
+const numStreams = 100
 
 type streamClient struct {
 	client mixmessages.Node_StreamPostPhaseClient
