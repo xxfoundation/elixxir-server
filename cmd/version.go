69093660a1a885a9dbc816e5ca38436fbb51b8ad
josh <josh@elixxir.io>
2021-07-15 16:05:13 -0700

    Increment minor version

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.5.2"
+const currentVersion = "2.6.0"
 
 func printVersion() {
 	fmt.Printf("xx network Server v%s -- %s\n\n", SEMVER, GITVERSION)
