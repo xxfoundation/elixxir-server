7ea1046ee2d94e93cb2239178fef5a5d6584b8e1
josh <josh@elixxir.io>
2021-07-26 16:53:06 -0700

    Remove authorizer in NotStarted

--- node/changeHandlers.go
+++ node/changeHandlers.go
@@ -67,10 +67,10 @@ func NotStarted(instance *internal.Instance) error {
 		return errors.Errorf("Unable to connect to registration server: %+v", err)
 	}
 
-	err = permissioning.Authorize(instance)
-	if err != nil {
-		return errors.Errorf("Could not authorize with network: %v", err)
-	}
+	//err = permissioning.Authorize(instance)
+	//if err != nil {
+	//	return errors.Errorf("Could not authorize with network: %v", err)
+	//}
 
 	// Connect to the Permissioning Server without authentication
 	params = connect.GetDefaultHostParams()
