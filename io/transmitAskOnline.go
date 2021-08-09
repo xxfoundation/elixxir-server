dddd3ebc8bd5e14835231325975ccc28a7c8574d
Richard T. Carback III <rick.carback@gmail.com>
2021-08-09 21:24:45 +0000

    Check if nodes are online before starting a new round

--- io/transmitAskOnline.go
+++ io/transmitAskOnline.go
@@ -10,15 +10,22 @@ package io
 // transmitAskOnline.go contains the logic for transmitting an askOnline comm
 
 import (
+	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/comms/node"
 	"gitlab.com/xx_network/comms/connect"
+	"sync/atomic"
 	"time"
 )
 
 // VerifyServersOnline Blocks until all given servers respond
-func VerifyServersOnline(network *node.Comms, servers *connect.Circuit) {
-	for i := 0; i < servers.Len(); {
+func VerifyServersOnline(network *node.Comms, servers *connect.Circuit,
+	timeoutDuration time.Duration) error {
+	var stop uint64
+	finished := make(chan bool, servers.Len())
+
+	// This helper runs until successfully connected or stop is not 0
+	askOnline := func(i int) {
 		// Pull server's host from the connection manager
 		serverID := servers.GetNodeAtIndex(i)
 		server := servers.GetHostAtIndex(i)
@@ -26,15 +33,46 @@ func VerifyServersOnline(network *node.Comms, servers *connect.Circuit) {
 		// Send AskOnline to all servers
 		jww.INFO.Printf("Waiting for cMix server %s (%d/%d)...",
 			serverID, i+1, servers.Len())
-		_, err := network.SendAskOnline(server)
-		if err != nil {
-			jww.WARN.Printf("Could not contact cMix server %s (%d/%d): %s...",
-				serverID, i+1, servers.Len(), err)
-			time.Sleep(250 * time.Millisecond)
-		} else {
-			i++
-			jww.INFO.Printf("cMix server %s (%d/%d) is online...",
+		for atomic.LoadUint64(&stop) == 0 {
+			_, err := network.SendAskOnline(server)
+			if err != nil {
+				jww.WARN.Printf("Could not contact "+
+					"cMix server %s (%d/%d): %+v",
+					serverID, i+1, servers.Len(),
+					err)
+				time.Sleep(250 * time.Millisecond)
+				continue
+			}
+			jww.INFO.Printf("cMix server %s (%d/%d) "+
+				"is online...",
 				serverID, i+1, servers.Len())
+			finished <- true
+			return
 		}
 	}
+
+	// Start goroutines to check liveness
+	for i := 0; i < servers.Len(); i++ {
+		go askOnline(i)
+	}
+
+	// Handle timeout and error reporting
+	var err error
+	err = nil
+	finishedCnt := 0
+	// We are done when all servers responded
+	for finishedCnt < servers.Len() && err == nil {
+		select {
+		case <-finished:
+			finishedCnt++
+			break
+		case <-time.After(timeoutDuration):
+			err = errors.Errorf("Timed out connecting to nodes")
+			break
+		}
+	}
+
+	// Close all goroutines
+	atomic.AddUint64(&stop, 1)
+	return err
 }
