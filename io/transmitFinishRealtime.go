7753d4c2b1bbd9c0cc9bf717b2330247fac68fe3
Benjamin Wenger <ben@elixxir.ioo>
2021-10-06 12:21:43 -0700

    improved a fix to transmitfinishRealtime to account for round data async and node state async

--- io/transmitFinishRealtime.go
+++ io/transmitFinishRealtime.go
@@ -78,11 +78,21 @@ func TransmitFinishRealtime(roundID id.Round, serverInstance phase.GenericInstan
 			var streamErr error
 			var ack *messages.Ack
 			for i:=0;i<3;i++{
+				localR, rnderr := instance.GetNetworkStatus().GetRound(roundID)
+				if rnderr!=nil{
+					streamErr = errors.Errorf("Could not get status of round %d in " +
+						"transmitFinishRealtime to %s on attempt %d/3",
+						roundID, recipient.GetId(), i)
+					jww.WARN.Printf(streamErr.Error())
+					time.Sleep(150*time.Millisecond)
+					continue
+				}
+				roundState := states.Round(localR.State)
 				localState := instance.GetStateMachine().Get()
-				if  localState != current.REALTIME  {
+				if  localState!=current.REALTIME || (roundState != states.QUEUED && roundState != states.REALTIME)  {
 					streamErr = errors.Errorf("The status of round %d in " +
 						"transmitFinishRealtime to %s on attempt %d/3 was not %s or %s, instead was %s",
-						roundID, recipient.GetId(), i, states.REALTIME, states.QUEUED, localState)
+						roundID, recipient.GetId(), i, states.REALTIME, states.QUEUED, roundState)
 					jww.WARN.Printf(streamErr.Error())
 					time.Sleep(150*time.Millisecond)
 					continue
